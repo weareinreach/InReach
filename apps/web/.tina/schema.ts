fe9b2b3169d9d5b59788b3582443f7b7f54f4b4f
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-09-10 18:34:07 -0400

    tina setup

--- apps/web/.tina/schema.ts
+++ apps/web/.tina/schema.ts
@@ -1,59 +1,60 @@
-
-import { defineSchema, defineConfig } from 'tinacms'
-import { client } from './__generated__/client'
-
+import { defineSchema, defineConfig, wrapFieldsWithMeta } from "tinacms";
+import { object } from "zod";
+import { client } from "./__generated__/client";
+import { navbar } from "./schemas";
 
 const branch =
   process.env.NEXT_PUBLIC_TINA_BRANCH ||
   process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
   process.env.HEAD ||
-  'main'
+  "main";
 const schema = defineSchema({
   // See https://tina.io/docs/tina-cloud/connecting-site/ for more information about this config
   config: {
-    token: '<Your Read Only Token>', // generated on app.tina.io,
-    clientId: '<Your Client ID>', // generated on app.tina.io
+    token: "<Your Read Only Token>", // generated on app.tina.io,
+    clientId: "<Your Client ID>", // generated on app.tina.io
     branch,
   },
   collections: [
+    navbar,
     {
-      label: 'Blog Posts',
-      name: 'post',
-      path: 'content/posts',
-      format: 'mdx',
+      label: "Blog Posts",
+      name: "post",
+      path: "content/posts",
+      format: "mdx",
       ui: {
         router: ({ document }) => {
           // This can be used to add contextual editing to your site. See https://tina.io/docs/tinacms-context/#accessing-contextual-editing-from-the-cms for more information.
-          return `/demo/blog/${document._sys.filename}`
+          return `/demo/blog/${document._sys.filename}`;
         },
       },
       fields: [
         {
-          type: 'string',
-          label: 'Title',
-          name: 'title',
+          type: "string",
+          label: "Title",
+          name: "title",
         },
         {
-          type: 'rich-text',
-          label: 'Blog Post Body',
-          name: 'body',
+          type: "rich-text",
+          label: "Blog Post Body",
+          name: "body",
           isBody: true,
           templates: [
             {
-              name: 'PageSection',
-              label: 'Page Section',
+              name: "PageSection",
+              label: "Page Section",
               fields: [
                 {
-                  type: 'string',
-                  name: 'heading',
-                  label: 'Heading',
+                  type: "string",
+                  name: "heading",
+                  label: "Heading",
                 },
                 {
-                  type: 'string',
-                  name: 'content',
-                  label: 'Content',
+                  type: "string",
+                  name: "content",
+                  label: "Content",
                   ui: {
-                    component: 'textarea',
+                    component: "textarea",
                   },
                 },
               ],
@@ -63,14 +64,13 @@ const schema = defineSchema({
       ],
     },
   ],
-})
+});
 
-export default schema
+export default schema;
 
 // Your tina config
 
 export const tinaConfig = defineConfig({
   client,
   schema,
-})
-
+});
--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app'
 // import Head from 'next/head'
+import Tina from '../../.tina/components/TinaDynamicProvider'
 import { MantineProvider } from '@inreach/ui/mantine/core'
 import { ModalsProvider } from '@inreach/ui/mantine/modals'
 import { NotificationsProvider } from '@inreach/ui/mantine/notifications'
 import { webTheme, webCache } from '@inreach/ui/theme'
-import Tina from '../../.tina/components/TinaDynamicProvider'
+import { WebLayout } from '@inreach/ui/layouts'
 
 const MyApp = (appProps: AppProps) => {
 	const { Component, pageProps } = appProps
@@ -18,7 +19,9 @@ const MyApp = (appProps: AppProps) => {
 			<NotificationsProvider>
 				<ModalsProvider>
 					<Tina>
-						<Component {...pageProps} />
+						<WebLayout>
+							<Component {...pageProps} />
+						</WebLayout>
 					</Tina>
 				</ModalsProvider>
 			</NotificationsProvider>
