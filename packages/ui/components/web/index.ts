ad06666178629a0ab6e6d03898e09c6c06eb599e
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-09-15 18:39:40 -0400

    feat: cms block components

---
+++ packages/ui/components/web/index.ts
@@ -0,0 +1,4 @@
+// Automatically generated index file by: https://marketplace.visualstudio.com/items?itemName=eliostruyf.vscode-typescript-exportallmodules
+
+export * from "./HeroCarousel";
+export * from "./LinkButton";
