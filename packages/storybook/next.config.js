50bf7393d13d56db57dd99a7c68af1ee0e481cec
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-09-22 16:01:08 -0400

    feat: add Storybook

---
+++ packages/storybook/next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+
+const withTM = require("next-transpile-modules")(["@inreach/ui"]);
+
+function defineNextConfig(config) {
+	return withTM(config);
+}
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+};
+
+module.exports = defineNextConfig(nextConfig);
