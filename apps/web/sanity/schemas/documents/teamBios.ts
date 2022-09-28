55b0d8b6bec757bf87f767070511b8f69dae01d5
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-09-28 08:54:23 -0400

    schema builds

--- apps/web/sanity/schemas/documents/teamBios.ts
+++ apps/web/sanity/schemas/documents/teamBios.ts
@@ -1,9 +1,11 @@
-import { defineField, defineType } from "sanity";
+import { SocialMediaIconSelect } from "@inreach/ui/components/web";
+import { defineArrayMember, defineField, defineType } from "sanity";
 
-export const author = defineType({
-	name: "teamBios",
-	title: "Team Member Bios",
+export const teamBio = defineType({
+	name: "teamBio",
+	title: "Team Member Bio",
 	type: "document",
+	i18n: true,
 	fields: [
 		defineField({
 			name: "name",
@@ -20,6 +22,39 @@ export const author = defineType({
 			},
 		}),
 		defineField({
+			name: "class",
+			type: "string",
+			title: "Team member type",
+			options: {
+				list: [
+					{
+						value: "staff",
+						title: "Staff",
+					},
+					{
+						value: "intern",
+						title: "Intern",
+					},
+					{
+						value: "volunteer",
+						title: "Volunteer",
+					},
+					{
+						value: "board",
+						title: "Board of Directors",
+					},
+					{
+						value: "jrboard",
+						title: "Junior Board",
+					},
+					{
+						value: "advisory",
+						title: "Advisory Council",
+					},
+				],
+			},
+		}),
+		defineField({
 			name: "image",
 			title: "Image",
 			type: "image",
@@ -32,12 +67,48 @@ export const author = defineType({
 			title: "Bio",
 			type: "array",
 			of: [
-				{
+				defineArrayMember({
 					title: "Block",
 					type: "block",
 					styles: [{ title: "Normal", value: "normal" }],
 					lists: [],
-				},
+				}),
+			],
+		}),
+		defineField({
+			title: "Social Media Links",
+			name: "socialMediaLinks",
+			type: "array",
+			of: [
+				defineArrayMember({
+					type: "object",
+					fields: [
+						defineField({
+							name: "service",
+							title: "Service",
+							type: "string",
+						}),
+						{
+							name: "href",
+							title: "Profile Link",
+							type: "url",
+							validation: (Rule) =>
+								Rule.uri({
+									allowRelative: true,
+									scheme: ["https", "http"],
+								}),
+						},
+
+						defineField({
+							name: "icon",
+							title: "Icon",
+							type: "string",
+							components: {
+								input: SocialMediaIconSelect,
+							},
+						}),
+					],
+				}),
 			],
 		}),
 	],
