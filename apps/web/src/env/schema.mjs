76443c41e0958164f5d6504f40bc45b8cde36757
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-10-28 18:41:52 -0400

    package installs, misc

--- apps/web/src/env/schema.mjs
+++ apps/web/src/env/schema.mjs
@@ -1,29 +1,33 @@
 // @ts-check
-import { z } from "zod";
+import { z } from 'zod'
 
 /**
  * Specify your server-side environment variables schema here.
  * This way you can ensure the app isn't built with invalid env vars.
  */
-export const serverSchema = z.object({
-	// SERVERVAR: z.string(),
-	SENTRY_DSN: z.string(),
-	SANITY_TOKEN: z.string().optional(),
-	NODE_ENV: z.string(),
-});
+export const serverSchema = z
+	.object({
+		// SERVERVAR: z.string(),
+		SENTRY_DSN: z.string(),
+		SANITY_TOKEN: z.string().optional(),
+		NODE_ENV: z.string(),
+	})
+	.deepPartial()
 
 /**
  * Specify your client-side environment variables schema here.
  * This way you can ensure the app isn't built with invalid env vars.
  * To expose them to the client, prefix them with `NEXT_PUBLIC_`.
  */
-export const clientSchema = z.object({
-	// NEXT_PUBLIC_CLIENTVAR: z.string(),
-	NEXT_PUBLIC_SENTRY_DSN: z.string(),
-	NEXT_PUBLIC_SANITY_PROJECT_ID: z.string(),
-	NEXT_PUBLIC_SANITY_DATASET: z.string(),
-	NEXT_PUBLIC_SANITY_API_VERSION: z.string(),
-});
+export const clientSchema = z
+	.object({
+		// NEXT_PUBLIC_CLIENTVAR: z.string(),
+		NEXT_PUBLIC_SENTRY_DSN: z.string(),
+		NEXT_PUBLIC_SANITY_PROJECT_ID: z.string(),
+		NEXT_PUBLIC_SANITY_DATASET: z.string(),
+		NEXT_PUBLIC_SANITY_API_VERSION: z.string(),
+	})
+	.deepPartial()
 
 /**
  * You can't destruct `process.env` as a regular object, so you have to do
@@ -37,4 +41,4 @@ export const clientEnv = {
 	NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
 	NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
 	NEXT_PUBLIC_SANITY_API_VERSION: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
-};
+}
