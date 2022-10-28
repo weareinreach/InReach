72704ddaa004a96966d53b11613a424bd683bb61
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-10-28 11:47:42 -0400

    shared eslint config

---
+++ apps/web/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	// "parser": "@typescript-eslint/parser",
+	parserOptions: {
+		project: "./tsconfig.json",
+	},
+	// "plugins": ["@typescript-eslint"],
+	root: true,
+	extends: [
+		"@inreach/eslint-config" /** ,"plugin:@typescript-eslint/recommended"*/,
+	],
+};
