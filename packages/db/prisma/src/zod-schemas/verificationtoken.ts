237fd96db312f3bf790060395983e1030240b8cf
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-11-08 12:20:45 -0500

    fix path

--- packages/db/prisma/src/zod-schemas/verificationtoken.ts
+++
@@ -1,9 +0,0 @@
-import * as z from 'zod'
-
-import * as imports from '../zod-util'
-
-export const _VerificationTokenModel = z.object({
-	identifier: z.string(),
-	token: z.string(),
-	expires: z.date(),
-})
