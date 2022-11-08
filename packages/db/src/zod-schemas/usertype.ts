9f360c3c793848fb7bc6f6cda9b7adab80310482
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-11-08 17:19:24 -0500

    roll back next to 13.0.1

---
+++ packages/db/src/zod-schemas/usertype.ts
@@ -0,0 +1,35 @@
+import * as z from 'zod'
+
+import * as imports from '../zod-util'
+import { CompleteTranslationKey, CompleteUser, TranslationKeyModel, UserModel } from './index'
+
+export const _UserTypeModel = z.object({
+	id: imports.cuid,
+	type: z.string(),
+	translationKeyId: imports.cuid,
+	createdAt: z.date(),
+	createdById: z.string().nullish(),
+	updatedAt: z.date(),
+	updatedById: z.string().nullish(),
+})
+
+export interface CompleteUserType extends z.infer<typeof _UserTypeModel> {
+	users: CompleteUser[]
+	translationKey: CompleteTranslationKey
+	createdBy?: CompleteUser | null
+	updatedBy?: CompleteUser | null
+}
+
+/**
+ * UserTypeModel contains all relations on your model in addition to the scalars
+ *
+ * NOTE: Lazy required in case of potential circular dependencies within schema
+ */
+export const UserTypeModel: z.ZodSchema<CompleteUserType> = z.lazy(() =>
+	_UserTypeModel.extend({
+		users: UserModel.array(),
+		translationKey: TranslationKeyModel,
+		createdBy: UserModel.nullish(),
+		updatedBy: UserModel.nullish(),
+	})
+)
