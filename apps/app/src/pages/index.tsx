2cc60df183b182d39d1f9995c766eeda952a8b47
Joe Karow <hello@joekarow.dev>
2022-11-11 21:09:54 -0500

    Auto stash before rebase of "origin/IN-687-base-layout"

--- apps/app/src/pages/index.tsx
+++ apps/app/src/pages/index.tsx
@@ -1,18 +1,20 @@
+import { useTranslation } from 'next-i18next'
+
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
 import { Button } from '@weareinreach/ui/mantine/core'
 
 const Home: NextPage = () => {
+	const { t } = useTranslation('common')
 	return (
 		<>
 			<Head>
-				{/* eslint-disable-next-line i18next/no-literal-string */}
-				<title>InReach</title>
+				<title>{t('inreach')}</title>
 			</Head>
 
-			<p>Hello world.</p>
-			<Button>I am a button</Button>
+			<p>{t('helloWorld')}</p>
+			<Button>{t('iAmAButton')}</Button>
 		</>
 	)
 }
