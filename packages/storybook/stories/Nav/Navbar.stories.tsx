e4d96990bfd2f0ef8f27cc611faab8c457bcea57
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-11-14 13:45:28 -0500

    storybook

---
+++ packages/storybook/stories/Nav/Navbar.stories.tsx
@@ -0,0 +1,41 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import React from 'react'
+
+import { Nav } from '@weareinreach/ui/components/layout'
+
+export default {
+	title: 'App/Navigation/Navigation Bar',
+	component: Nav,
+	// argTypes: {
+	// 	navItems: {
+	// 		type: 'symbol',
+
+	// 	}
+	// }
+} as ComponentMeta<typeof Nav>
+
+const Template: ComponentStory<typeof Nav> = (args) => <Nav {...args} />
+
+export const Primary = Template.bind({})
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+	navItems: [
+		{
+			key: 'About Us',
+			href: '#aboutus',
+		},
+		{
+			key: 'Take Action',
+			href: '#takeaction',
+		},
+		{
+			key: 'Questions?',
+			href: '#questions',
+		},
+		{
+			key: 'Contact Us',
+			href: '#contactus',
+		},
+	],
+}
---
+++ packages/storybook/stories/Nav/UserMenu.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { SessionProvider } from 'next-auth/react'
+
+import React from 'react'
+
+import { UserMenu } from '@weareinreach/ui/components/layout/Nav'
+
+export default {
+	title: 'App/Navigation/UserMenu',
+	component: UserMenu,
+	args: {},
+} as ComponentMeta<typeof UserMenu>
+
+const Template: ComponentStory<typeof UserMenu> = () => <UserMenu />
+
+export const Story = Template.bind({})
+Story.args = {}
