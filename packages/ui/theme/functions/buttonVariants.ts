dcc352bedeba989533ec4145eef6ebd22593c002
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-11-15 18:34:49 -0500

    theme colors, button variants

---
+++ packages/ui/theme/functions/buttonVariants.ts
@@ -0,0 +1,34 @@
+import type { ButtonStylesParams, CSSObject, MantineTheme } from '@mantine/core'
+
+type ButtonVariants = (theme: MantineTheme, params: ButtonStylesParams) => Record<string, CSSObject>
+
+export const buttonVariants: ButtonVariants = (theme, params) => {
+	switch (params.variant) {
+		case 'filled':
+			return {
+				root: {
+					backgroundColor: theme.colors.inReachPrimaryRegular[5],
+					'&:hover': {
+						backgroundColor: theme.colors.inReachPrimaryHover[5],
+					},
+				},
+				inner: {},
+			}
+		case 'outline':
+			return {
+				root: {
+					outlineColor: theme.colors.inReachPrimaryRegular[5],
+					'&:hover': {
+						outlineColor: theme.colors.inReachPrimaryHover[5],
+					},
+				},
+				inner: {
+					color: theme.colors.primaryText[9],
+				},
+			}
+	}
+	return {
+		root: {},
+		inner: {},
+	}
+}
---
+++ packages/ui/theme/functions/index.ts
@@ -0,0 +1,3 @@
+// codegen:start {preset: barrel, include: ./*.ts*}
+export * from './buttonVariants'
+// codegen:end
