3473dbb21a527ccf846574d89a688b0bfbd1d149
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-11-22 17:01:23 -0500

    add more languages

--- packages/db/seed/data/02-languages.ts
+++ packages/db/seed/data/02-languages.ts
@@ -1,92 +1,35 @@
 import { Prisma } from '@prisma/client'
-
-import { prisma } from '~/index'
+import ISO6391 from 'iso-google-locales'
 
 import { connectUser } from './01-user'
 
-const languages = [
-	{
-		localeCode: 'en-US',
-		iso6392: 'eng',
-		languageName: 'English (US)',
-		nativeName: 'English (US)',
-	},
-	{
-		localeCode: 'en-CA',
-		iso6392: 'eng',
-		languageName: 'English (CA)',
-		nativeName: 'English (CA)',
-	},
-	{
-		localeCode: 'en-MX',
-		iso6392: 'eng',
-		languageName: 'English (MX)',
-		nativeName: 'English (MX)',
-	},
-	{
-		localeCode: 'es',
-		iso6392: 'spa',
-		languageName: 'Spanish',
-		nativeName: 'Español',
-	},
-	{
-		localeCode: 'es-US',
-		iso6392: 'spa',
-		languageName: 'Spanish (US)',
-		nativeName: 'Español (EE)',
-	},
-	{
-		localeCode: 'es-CA',
-		iso6392: 'spa',
-		languageName: 'Spanish (CA)',
-		nativeName: 'Español (CA)',
-	},
-	{
-		localeCode: 'es-MX',
-		iso6392: 'spa',
-		languageName: 'Spanish (MX)',
-		nativeName: 'Español (MX)',
-	},
-]
+const activeTranslations = ['en', 'en-US', 'en-CA', 'es', 'es-MX', 'es-US']
+
+const languages = ISO6391.getAllCodes()
 
 // en is already created during the initial user seed
 export const seedLanguageData: Prisma.LanguageUpsertArgs[] = languages.map((lang) => {
-	const { localeCode, iso6392, languageName, nativeName } = lang
-
+	const localeCode = lang
+	const languageName = ISO6391.getName(lang)
+	const nativeName = ISO6391.getNativeName(lang)
+	const activelyTranslated = activeTranslations.includes(lang)
 	return {
 		where: {
 			localeCode,
 		},
 		create: {
 			localeCode,
-			iso6392,
 			languageName,
 			nativeName,
+			activelyTranslated,
 			createdBy: connectUser,
 			updatedBy: connectUser,
 		},
 		update: {
-			iso6392,
 			languageName,
 			nativeName,
+			activelyTranslated,
 			updatedBy: connectUser,
 		},
 	}
 })
-
-export const getPrimaryLanguages = async () =>
-	await prisma.language.findMany({
-		where: {
-			iso6392: {
-				not: null,
-			},
-			primary: true,
-		},
-		select: {
-			id: true,
-			iso6392: true,
-			localeCode: true,
-		},
-	})
-
-export type PrimaryLanguages = Prisma.PromiseReturnType<typeof getPrimaryLanguages>
