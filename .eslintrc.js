0a7107a0ddc5484eb53f51cbc9af8163aae0492b
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-11-24 00:51:10 -0500

    fix linting issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
 	// root: true,
-	// // This tells ESLint to load the config from the package `eslint-config-custom`
-	// extends: ['@weareinreach/eslint-config'],
+	// extends: [ '@weareinreach/eslint-config' ],
 	// settings: {
 	// 	next: {
 	// 		rootDir: ['apps/*/'],
