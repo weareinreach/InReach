78186b068e5bd507830d9eaaf8e7ca46eef2c251
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-12-01 02:44:56 -0500

    cleanup files

--- packages/db/prisma/migrations/20221122213113_/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `primary` on the `Language` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Language" DROP COLUMN "primary",
-ADD COLUMN     "activelyTranslated" BOOLEAN NOT NULL DEFAULT false;
--- packages/db/prisma/migrations/20221122223840_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Attribute" ADD COLUMN     "requireCountry" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "requireLanguage" BOOLEAN NOT NULL DEFAULT false;
