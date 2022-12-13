89851499bd962824e7660f69278acd0002f53224
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2022-12-13 15:27:26 -0800

    Base Appearance Complete

--- packages/ui/theme/functions/buttonVariants.ts
+++ packages/ui/theme/functions/buttonVariants.ts
@@ -1,6 +1,12 @@
-import type { ButtonStylesParams, CSSObject, MantineTheme } from '@mantine/core'
+import { ButtonVariant, CSSObject, Flex, MantineTheme } from '@mantine/core'
 
 type ButtonVariants = (theme: MantineTheme, params: ButtonStylesParams) => Record<string, CSSObject>
+
+type CustomVariants = 'large-primary' | 'large-secondary' | 'large-accent'
+
+interface ButtonStylesParams {
+	variant: ButtonVariant | CustomVariants
+}
 
 export const buttonVariants: ButtonVariants = (theme, params) => {
 	switch (params.variant) {
@@ -23,6 +29,119 @@ export const buttonVariants: ButtonVariants = (theme, params) => {
 					},
 				},
 				inner: {},
+			}
+		case 'large-primary':
+			return {
+				root: {
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: '#000000',
+					borderRadius: theme.radius.md,
+					padding: '6px 48px',
+					gap: theme.spacing.sm,
+					width: 'hug',
+					height: '48px',
+					left: theme.spacing.lg,
+					top: theme.spacing.lg,
+
+					'&:hover': {
+						background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.4), rgba(255, 255, 255, 0.4)), #000000',
+					},
+				},
+				inner: {
+					color: '#ffffff',
+					fontWeight: '500',
+					size: theme.fontSizes.md,
+				},
+				label: {
+					textTransform: 'none',
+					width: 'hug',
+					height: 'hug',
+					top: '14px',
+					left: '80px',
+				},
+				leftIcon: {
+					height: '24px',
+					width: theme.spacing.lg,
+				},
+			}
+		case 'large-secondary':
+			return {
+				root: {
+					border: '1px solid #d9d9d9',
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: '#FFFFFF',
+					borderRadius: theme.radius.md,
+					padding: '6px 48px',
+					gap: theme.spacing.sm,
+					width: 'hug',
+					height: '48px',
+					left: theme.spacing.lg,
+					top: theme.spacing.lg,
+
+					'&:hover': {
+						background: '#EFEFEF',
+					},
+				},
+				inner: {
+					color: '#000000',
+					fontWeight: '500',
+					size: theme.fontSizes.md,
+				},
+				label: {
+					textTransform: 'none',
+					width: 'hug',
+					height: 'hug',
+					top: '14px',
+					left: '80px',
+				},
+				leftIcon: {
+					height: '24px',
+					width: theme.spacing.lg,
+				},
+			}
+		case 'large-accent':
+			return {
+				root: {
+					display: 'flex',
+					flexDirection: 'row',
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: theme.colors.inReachSecondaryRegular[5],
+					borderRadius: theme.radius.xl,
+					padding: '6px 48px',
+					gap: theme.spacing.sm,
+					width: 'hug',
+					height: '48px',
+					left: theme.spacing.lg,
+					top: theme.spacing.lg,
+
+					'&:hover': {
+						background: 'linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), #4792DA;',
+					},
+				},
+				inner: {
+					color: theme.colors.primaryText[0],
+					fontWeight: '500',
+					fontSize: theme.fontSizes.md,
+				},
+				label: {
+					textTransform: 'none',
+					width: 'hug',
+					height: 'hug',
+					top: '14px',
+					left: '80px',
+					lineHeight: '125%',
+				},
+				leftIcon: {
+					height: '24px',
+					width: theme.spacing.lg,
+				},
 			}
 	}
 	return {
