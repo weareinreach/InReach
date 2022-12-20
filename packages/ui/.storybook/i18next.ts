77544d7e135fdd1fe4ed125871c51112b7f7489e
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2022-12-20 13:23:28 -0500

    fix formatting

--- packages/ui/.storybook/i18next.ts
+++ packages/ui/.storybook/i18next.ts
@@ -1,35 +1,32 @@
-import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import Backend from 'i18next-http-backend'
+import { initReactI18next } from 'react-i18next'
 
-const ns = ["common", "country", "user", "footer", "nav", "socialMedia"];
+const ns = ['common', 'country', 'user', 'footer', 'nav', 'socialMedia']
 export const i18nLocales = {
-	en: "English",
+	en: 'English',
 	// 'en-US': 'English (US)',
 	// 'en-CA': 'English (CA)',
 	// 'en-MX': 'English (MX)',
 	// es: 'Spanish',
 	// 'es-US': 'Spanish (US)',
 	// 'es-MX': 'Spanish (MX)',
-};
+}
 // const supportedLngs = ['en-US', 'en-CA', 'en-MX', 'es', 'es-US', 'es-MX']
-const supportedLngs = Object.keys(i18nLocales);
+const supportedLngs = Object.keys(i18nLocales)
 
 // const localePath = (lng: string, n: string) => `../../../apps/app/public/locales/${lng}/${n}.json`
-const resources = ns.reduce(
-	(acc: Record<string, Record<string, string>>, n) => {
-		supportedLngs.forEach((lng) => {
-			if (!acc[lng]) acc[lng] = {};
-			acc[lng] = {
-				...acc[lng],
-				[n]: require(`../../../apps/app/public/locales/${lng}/${n}.json`),
-			};
-		});
-		return acc;
-	},
-	{}
-);
+const resources = ns.reduce((acc: Record<string, Record<string, string>>, n) => {
+	supportedLngs.forEach((lng) => {
+		if (!acc[lng]) acc[lng] = {}
+		acc[lng] = {
+			...acc[lng],
+			[n]: require(`../../../apps/app/public/locales/${lng}/${n}.json`),
+		}
+	})
+	return acc
+}, {})
 
 i18n
 	.use(LanguageDetector)
@@ -37,17 +34,17 @@ i18n
 	.use(initReactI18next)
 	.init({
 		debug: true,
-		lng: "en",
+		lng: 'en',
 		fallbackLng: {
-			en: ["en-US"],
-			es: ["es-US"],
+			en: ['en-US'],
+			es: ['es-US'],
 		},
-		defaultNS: "common",
+		defaultNS: 'common',
 		ns,
 		interpolation: { escapeValue: false },
 		react: { useSuspense: false },
 		supportedLngs,
 		resources,
-	});
+	})
 
-export { i18n };
+export { i18n }
