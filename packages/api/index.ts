7bded00bd7ac055a7340ff0182887dee7debaffd
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-01-10 18:03:55 -0500

    export types

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,5 +1,12 @@
+import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
+
+import type { AppRouter } from './src/router'
+
 export type { AppRouter } from './src/router'
 export { appRouter } from './src/router'
 
 export { createContext } from './src/context'
 export type { Context } from './src/context'
+
+export type ApiInput = inferRouterInputs<AppRouter>
+export type ApiOutput = inferRouterOutputs<AppRouter>
--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,10 +1,12 @@
 import { router } from '../trpc'
 import { authRouter } from './auth'
 import { exampleRouter } from './example'
+import { userRouter } from './user'
 
 export const appRouter = router({
 	example: exampleRouter,
 	auth: authRouter,
+	user: userRouter,
 })
 
 // export type definition of API
