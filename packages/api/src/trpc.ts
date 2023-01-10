a56fc4c7a2e5b86ddd6f17f779e2adcb6fb55de8
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-01-10 17:11:59 -0500

    trpc role based middleware

--- packages/api/src/trpc.ts
+++ packages/api/src/trpc.ts
@@ -11,13 +11,26 @@ const t = initTRPC.context<Context>().create({
 })
 
 /** Reusable middleware to ensure users are logged in */
+const reject = () => {
+	throw new TRPCError({ code: 'UNAUTHORIZED' })
+}
 const isAuthed = t.middleware(({ ctx, next }) => {
 	if (!ctx.session || !ctx.session.user) {
-		throw new TRPCError({ code: 'UNAUTHORIZED' })
+		return reject()
 	}
 	return next({
 		ctx: {
 			// infers the `session` as non-nullable
+			session: { ...ctx.session, user: ctx.session.user },
+		},
+	})
+})
+const isAdmin = t.middleware(({ ctx, next }) => {
+	if (!ctx.session || !ctx.session.user) return reject()
+	if (!['dataAdmin', 'sysadmin'].includes(ctx.session?.user.role)) return reject()
+
+	return next({
+		ctx: {
 			session: { ...ctx.session, user: ctx.session.user },
 		},
 	})
@@ -30,3 +43,6 @@ export const publicProcedure = t.procedure
 
 /** Protected procedure */
 export const protectedProcedure = t.procedure.use(isAuthed)
+
+/** Admin procedure */
+export const adminProcedure = t.procedure.use(isAdmin)
--- packages/auth/next-auth.d.ts
+++ packages/auth/next-auth.d.ts
@@ -11,6 +11,7 @@ declare module 'next-auth' {
 	interface Session extends DefaultSession {
 		user: {
 			id: string
+			role: string
 		} & DefaultSession['user']
 	}
 }
