9f07d8a4185239c135eb6f257d98a5ab125134d8
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-01-11 15:09:22 -0500

    remove example

--- packages/api/router/auth.ts
+++ packages/api/router/auth.ts
@@ -1,10 +1,7 @@
-import { defineRouter, protectedProcedure, publicProcedure } from '../lib/trpc'
+import { defineRouter, publicProcedure } from '../lib/trpc'
 
 export const authRouter = defineRouter({
 	getSession: publicProcedure.query(({ ctx }) => {
 		return ctx.session
-	}),
-	getSecretMessage: protectedProcedure.query(() => {
-		return 'You are logged in and can see this secret message!'
 	}),
 })
--- packages/api/router/index.ts
+++ packages/api/router/index.ts
@@ -1,11 +1,9 @@
 import { defineRouter } from '../lib/trpc'
 import { authRouter } from './auth'
-import { exampleRouter } from './example'
 import { savedListRouter } from './savedLists'
 import { userRouter } from './user'
 
 export const appRouter = defineRouter({
-	example: exampleRouter,
 	auth: authRouter,
 	user: userRouter,
 	savedList: savedListRouter,
