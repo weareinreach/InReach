9cf7b8cafe0c69944fc47a1fb63933491d341eda
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-01-18 20:20:45 -0500

    i18n/general config

--- packages/ui/.storybook/i18next.ts
+++ packages/ui/.storybook/i18next.ts
@@ -1,9 +1,9 @@
 import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
-import Backend from 'i18next-http-backend'
+import HttpApi, { HttpBackendOptions } from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
 
-const ns = ['common', 'country', 'user', 'footer', 'nav', 'socialMedia']
+const ns = ['common', 'country', 'user', 'footer', 'nav', 'socialMedia', 'attribute']
 export const i18nLocales = {
 	en: 'English',
 	// 'en-US': 'English (US)',
@@ -16,37 +16,40 @@ export const i18nLocales = {
 // const supportedLngs = ['en-US', 'en-CA', 'en-MX', 'es', 'es-US', 'es-MX']
 const supportedLngs = Object.keys(i18nLocales)
 
-// const localePath = (lng: string, n: string) => `../../../apps/app/public/locales/${lng}/${n}.json`
-const resources = ns.reduce((acc: Record<string, Record<string, string>>, n) => {
-	supportedLngs.forEach((lng) => {
-		if (!acc[lng]) acc[lng] = {}
-		acc[lng] = {
-			...acc[lng],
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-			[n]: require(`../../../apps/app/public/locales/${lng}/${n}.json`),
-		}
-	})
-	return acc
-}, {})
+// const resources = ns.reduce((acc: Record<string, Record<string, string>>, n) => {
+// 	supportedLngs.forEach((lng) => {
+// 		if (!acc[lng]) acc[lng] = {}
+// 		acc[lng] = {
+// 			...acc[lng],
+// 			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+// 			[n]: require(`../../../apps/app/public/locales/${lng}/${n}.json`),
+// 		}
+// 	})
+// 	return acc
+// }, {})
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
 i18n
 	.use(LanguageDetector)
-	.use(Backend)
+	.use(HttpApi)
 	.use(initReactI18next)
-	.init({
+	.init<HttpBackendOptions>({
 		debug: true,
 		lng: 'en',
+		backend: {
+			loadPath: '/public/locales/{{lng}}/{{ns}}.json',
+		},
 		fallbackLng: {
-			en: ['en-US'],
-			es: ['es-US'],
+			'en-US': ['en'],
+			'es-US': ['es'],
 		},
 		defaultNS: 'common',
-		ns,
 		interpolation: { escapeValue: false },
 		react: { useSuspense: false },
+		cleanCode: true,
 		supportedLngs,
-		resources,
+		ns,
+		// resources,
 	})
 
 export { i18n }
--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,11 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import { type StorybookConfig } from '@storybook/core-common'
+import { merge } from 'merge-anything'
+
 import * as path from 'path'
 
 const filePattern = '*.stories.@(js|jsx|ts|tsx|mdx)'
 
-const config = {
+const config: StorybookConfig = {
 	stories: [`../components/**/${filePattern}`, `../layout/**/${filePattern}`],
-	staticDirs: ['../../../apps/app/public'],
+	staticDirs: [{ from: '../../../apps/app/public', to: 'public/' }],
 	addons: [
 		'@geometricpanda/storybook-addon-badges',
 		'@storybook/addon-a11y',
@@ -35,87 +38,50 @@ const config = {
 		reactDocgen: 'react-docgen-typescript',
 		reactDocgenTypescriptOptions: {
 			shouldExtractLiteralValuesFromEnum: true,
-			// propFilter: (prop: Record<string, any>) =>
-			// 	prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
 		},
 	},
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	webpackFinal: (config: Record<string, any>) => {
-		/** Next-Auth session mock */
-		// eslint-disable-next-line @typescript-eslint/no-extra-semi
-		;(config.resolve.alias['@tomfreudenberg/next-auth-mock/storybook/preview-mock-auth-states'] =
-			path.resolve(__dirname, 'mockAuthStates.ts')),
-			(config.resolve.alias['next-i18next'] = 'react-i18next')
-		/**
-		 * Fixes font import with /
-		 *
-		 * @see https://github.com/storybookjs/storybook/issues/12844#issuecomment-867544160
-		 */
-		// config.resolve.roots = [
-		// 	path.resolve(__dirname, "../public"),
-		// 	"node_modules",
-		// ];
-		// config.resolve.alias = {
-		// 	...config.resolve.alias,
-		// 	"@/interfaces": path.resolve(__dirname, "../interfaces"),
-		// };
-		/**
-		 * Why webpack5... Just why?
-		 *
-		 * @type {{
-		 * 	console: boolean
-		 * 	process: boolean
-		 * 	timers: boolean
-		 * 	os: boolean
-		 * 	querystring: boolean
-		 * 	sys: boolean
-		 * 	fs: boolean
-		 * 	url: boolean
-		 * 	crypto: boolean
-		 * 	path: boolean
-		 * 	zlib: boolean
-		 * 	punycode: boolean
-		 * 	util: boolean
-		 * 	stream: boolean
-		 * 	assert: boolean
-		 * 	string_decoder: boolean
-		 * 	domain: boolean
-		 * 	vm: boolean
-		 * 	tty: boolean
-		 * 	http: boolean
-		 * 	buffer: boolean
-		 * 	constants: boolean
-		 * 	https: boolean
-		 * 	events: boolean
-		 * }}
-		 */
-		config.resolve.fallback = {
-			fs: false,
-			assert: false,
-			buffer: false,
-			console: false,
-			constants: false,
-			crypto: false,
-			domain: false,
-			events: false,
-			http: false,
-			https: false,
-			os: false,
-			path: false,
-			punycode: false,
-			process: false,
-			querystring: false,
-			stream: false,
-			string_decoder: false,
-			sys: false,
-			timers: false,
-			tty: false,
-			url: false,
-			util: false,
-			vm: false,
-			zlib: false,
+	webpackFinal: (config) => {
+		const configAdditions: typeof config = {
+			resolve: {
+				alias: {
+					/** Next-Auth session mock */
+					'@tomfreudenberg/next-auth-mock/storybook/preview-mock-auth-states': path.resolve(
+						__dirname,
+						'mockAuthStates.ts'
+					),
+					'next-i18next': 'react-i18next',
+				},
+				roots: [path.resolve(__dirname, '../../../apps/app/public')],
+				fallback: {
+					fs: false,
+					assert: false,
+					buffer: false,
+					console: false,
+					constants: false,
+					crypto: false,
+					domain: false,
+					events: false,
+					http: false,
+					https: false,
+					os: false,
+					path: false,
+					punycode: false,
+					process: false,
+					querystring: false,
+					stream: false,
+					string_decoder: false,
+					sys: false,
+					timers: false,
+					tty: false,
+					url: false,
+					util: false,
+					vm: false,
+					zlib: false,
+				},
+			},
 		}
-		return config
+		const mergedConfig = merge(config, configAdditions)
+		return mergedConfig
 	},
 }
 export default config
