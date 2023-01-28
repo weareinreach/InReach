8de201e2c0391b8adcc2f0f9d2cada31e720e8bd
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-01-28 01:50:51 -0500

    testing file

--- apps/app/src/pages/datatest.tsx
+++ apps/app/src/pages/datatest.tsx
@@ -1,39 +1,35 @@
+import { Code } from '@mantine/core'
 import { trpcServerClient } from '@weareinreach/api/trpc'
-import { UserReviewPrompt } from '@weareinreach/ui/components/core'
-import { DateTime } from 'luxon'
 import { GetServerSidePropsContext, NextPage } from 'next'
 import { useSession } from 'next-auth/react'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
 
 import { api } from '~/utils/api'
 
-const DataTest: NextPage = () => {
-	const x = api.organization.getById.useQuery({ id: 'cldax0nbr00jev1mqeup963jo' })
-	// const time = DateTime.fromJSDate(x.data?.hours[0]?.end)
-	// 	//.set({ weekday: x.data?.hours[0]?.dayIndex })
-	// 	.toLocaleString(DateTime.TIME_24_WITH_SHORT_OFFSET)
+const DataTest: NextPage = (props) => {
+	const x = api.organization.searchName.useQuery({ search: 'trevor' })
 
 	return (
 		<>
-			{JSON.stringify(useSession(), null, 2)}
-			<br />
-			<br />
-			{JSON.stringify(api.auth.getSession.useQuery(), null, 2)}
-			<UserReviewPrompt avatarName='' avatarUrl='' submitHandler={y} />
+			<Code block>{JSON.stringify(useSession(), null, 2)}</Code>
+			<Code block>{JSON.stringify(x, null, 2)}</Code>
+			<Code block>{JSON.stringify(props, null, 2)}</Code>
 		</>
 	)
 }
 
-export const getServerSideProps = async (context: GetServerSidePropsContext) => {
-	const { req, res } = context
-
+export const getServerSideProps = async ({ req, res, locale }: GetServerSidePropsContext) => {
 	const ssg = await trpcServerClient()
 
 	await ssg.organization.searchName.prefetch({ search: 'trevor' })
 
+	const props = {
+		trpcState: ssg.dehydrate(),
+		...(await serverSideTranslations(locale as string, ['common'])),
+	}
+
 	return {
-		props: {
-			trpcState: ssg.dehydrate(),
-		},
+		props,
 	}
 }
 
