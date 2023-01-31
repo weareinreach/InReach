cd6758d24f7d11c26063f69bc7e8c9c811524caa
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-01-31 10:18:20 -0500

    comment out until trpc/trpc#3700 is released

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,4 +1,4 @@
-import { type inferRouterProxyClient } from '@trpc/client'
+// import { type inferRouterProxyClient } from '@trpc/client'
 import { type inferReactQueryProcedureOptions } from '@trpc/react-query'
 
 import { type AppRouter } from './router'
@@ -12,4 +12,4 @@ export { createContext } from './lib/context'
 export type { Context } from './lib/context'
 
 export type ReactQueryOptions = inferReactQueryProcedureOptions<AppRouter>
-export type RouterProxyClient = inferRouterProxyClient<AppRouter>
+// export type RouterProxyClient = inferRouterProxyClient<AppRouter>
