11bbb0e8336c64e4c2ba058c9038bfdc6dc3a95d
trigal <10123216+trigal2012@users.noreply.github.com>
2023-01-31 08:26:47 -0800

    update stoybook component

--- packages/ui/components/core/ActionButtons.stories.tsx
+++ packages/ui/components/core/ActionButtons.stories.tsx
@@ -1,5 +1,5 @@
 import { Center } from '@mantine/core'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Meta, StoryFn } from '@storybook/react'
 import React from 'react'
 
 import { ActionButtons as ActionButtonsComponent, actionButtonIcons } from './ActionButtons'
@@ -19,30 +19,10 @@ export default {
 			url: 'https://www.figma.com/file/gl8ppgnhpSq1Dr7Daohk55/Design-System-(2023)?node-id=51%3A472&t=MmGmrL63FUWcqBUe-0',
 		},
 	},
-} as ComponentMeta<typeof ActionButtonsComponent>
+} as Meta<typeof ActionButtonsComponent>
 
-const ActionButton: ComponentStory<typeof ActionButtonsComponent> = (args) => (
-	<ActionButtonsComponent {...args} />
-)
-
-export const ActionButtonSave = ActionButton.bind({})
-export const ActionButtonShare = ActionButton.bind({})
-export const ActionButtonPrint = ActionButton.bind({})
-export const ActionButtonDelete = ActionButton.bind({})
-export const ActionButtonReview = ActionButton.bind({})
-
-ActionButtonSave.args = {
-	iconKey: 'save',
-}
-ActionButtonShare.args = {
-	iconKey: 'share',
-}
-ActionButtonPrint.args = {
-	iconKey: 'print',
-}
-ActionButtonDelete.args = {
-	iconKey: 'delete',
-}
-ActionButtonReview.args = {
-	iconKey: 'review',
+export const ActionButton = {
+	args: {
+		iconKey: 'save',
+	},
 }
