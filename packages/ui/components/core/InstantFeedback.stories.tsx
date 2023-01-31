3c10f8333816cd916455a5517d7412e860b4af26
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-01-30 22:42:44 -0500

    update stories to CSFv3

--- packages/ui/components/core/InstantFeedback.stories.tsx
+++ packages/ui/components/core/InstantFeedback.stories.tsx
@@ -1,5 +1,5 @@
 import { Center } from '@mantine/core'
-import { ComponentMeta, ComponentStory } from '@storybook/react'
+import { Meta, StoryFn } from '@storybook/react'
 import React from 'react'
 
 import { InstantFeedback as InstantFeedbackCompnent } from './InstantFeedback'
@@ -13,19 +13,15 @@ export default {
 			url: 'https://www.figma.com/file/gl8ppgnhpSq1Dr7Daohk55/Design-System-(2023)?node-id=70%3A2310&t=gF8uAWa4xEOPSNAI-0',
 		},
 	},
-} as ComponentMeta<typeof InstantFeedbackCompnent>
+} as Meta<typeof InstantFeedbackCompnent>
 
-const InstantFeedbackStory: ComponentStory<typeof InstantFeedbackCompnent> = (args) => (
-	<InstantFeedbackCompnent {...args} />
-)
-
-export const InstantFeedback = InstantFeedbackStory.bind({})
-
-InstantFeedback.args = {
-	displayTextKey: 'resource-saved',
-	icon: 'heartFilled',
-	link: {
-		href: '#',
-		textKey: 'view-list',
+export const InstantFeedback = {
+	args: {
+		displayTextKey: 'resource-saved',
+		icon: 'heartFilled',
+		link: {
+			href: '#',
+			textKey: 'view-list',
+		},
 	},
 }
