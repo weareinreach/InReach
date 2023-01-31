0f3fc501698eaba0e5476165d10a4499404ed112
trigal <10123216+trigal2012@users.noreply.github.com>
2023-01-30 21:07:21 -0800

    all button options, not styled

--- packages/ui/components/core/ActionButtons.stories.tsx
+++ packages/ui/components/core/ActionButtons.stories.tsx
@@ -1,7 +1,8 @@
+import { Center } from '@mantine/core'
 import { ComponentMeta, ComponentStory } from '@storybook/react'
 import React from 'react'
 
-import { ActionButtons as ActionButtonsComponent } from './ActionButtons'
+import { ActionButtons as ActionButtonsComponent, approvedButtonIcons } from './ActionButtons'
 
 export default {
 	title: 'Design System/Action Buttons',
@@ -9,11 +10,33 @@ export default {
 	parameters: {
 		design: {
 			type: 'figma',
-			url: 'https://www.figma.com/file/gl8ppgnhpSq1Dr7Daohk55/Design-System-(2023)?node-id=51%3A472&t=5MSsVbY5MYzkQyw4-0',
+			url: 'https://www.figma.com/file/gl8ppgnhpSq1Dr7Daohk55/Design-System-(2023)?node-id=51%3A472&t=MmGmrL63FUWcqBUe-0',
 		},
 	},
 } as ComponentMeta<typeof ActionButtonsComponent>
 
-const ActionButtons: ComponentStory<typeof ActionButtonsComponent> = () => <ActionButtonsComponent />
+const ActionButton: ComponentStory<typeof ActionButtonsComponent> = (args) => (
+	<ActionButtonsComponent {...args} />
+)
 
-export const ActionButtonsOptions = ActionButtons.bind({})
+export const ActionButtonSave = ActionButton.bind({})
+export const ActionButtonShare = ActionButton.bind({})
+export const ActionButtonPrint = ActionButton.bind({})
+export const ActionButtonDelete = ActionButton.bind({})
+export const ActionButtonReview = ActionButton.bind({})
+
+ActionButtonSave.args = {
+	iconKey: 'save',
+}
+ActionButtonShare.args = {
+	iconKey: 'share',
+}
+ActionButtonPrint.args = {
+	iconKey: 'print',
+}
+ActionButtonDelete.args = {
+	iconKey: 'delete',
+}
+ActionButtonReview.args = {
+	iconKey: 'review',
+}
--- packages/ui/components/core/ActionButtons.tsx
+++ packages/ui/components/core/ActionButtons.tsx
@@ -1,57 +1,35 @@
-import { Button, Menu, Text, useMantineTheme } from '@mantine/core'
-import { IconSquareCheck, IconPackage, IconUsers, IconCalendar, IconChevronDown } from '@tabler/icons'
+import { Button, createStyles, Menu, Text, useMantineTheme } from '@mantine/core'
+import { useTranslation } from 'next-i18next'
 
-export function ActionButtons() {
+import { Icon } from '../../icon'
+
+export const approvedButtonIcons = {
+	save: { icon: 'carbon:favorite', labelKey: 'save' },
+	share: { icon: 'carbon:share', labelKey: 'share' },
+	print: { icon: 'carbon:printer', labelKey: 'print' },
+	delete: { icon: 'carbon:delete', labelKey: 'delete' },
+	review: { icon: 'carbon:star', labelKey: 'review' },
+} as const
+
+const useStyles = createStyles((theme) => ({
+	button: {
+		color: theme.other.colors.secondary.black,
+
+		'&:hover': {
+			backgroundColor: theme.other.colors.primary.lightGray,
+		},
+	},
+}))
+
+export const ActionButtons = ({ iconKey }: Props) => {
+	const { classes } = useStyles()
 	const theme = useMantineTheme()
-	return (
-		<Menu transition='pop-top-right' position='top-end' width={220} withinPortal>
-			<Menu.Target>
-				<Button rightIcon={<IconChevronDown size={18} stroke={1.5} />} pr={12}>
-					Create new
-				</Button>
-			</Menu.Target>
-			<Menu.Dropdown>
-				<Menu.Item
-					icon={<IconPackage size={16} color={theme.colors.blue[6]} stroke={1.5} />}
-					rightSection={
-						<Text size='xs' transform='uppercase' weight={700} color='dimmed'>
-							Ctrl + P
-						</Text>
-					}
-				>
-					Project
-				</Menu.Item>
-				<Menu.Item
-					icon={<IconSquareCheck size={16} color={theme.colors.pink[6]} stroke={1.5} />}
-					rightSection={
-						<Text size='xs' transform='uppercase' weight={700} color='dimmed'>
-							Ctrl + T
-						</Text>
-					}
-				>
-					Task
-				</Menu.Item>
-				<Menu.Item
-					icon={<IconUsers size={16} color={theme.colors.cyan[6]} stroke={1.5} />}
-					rightSection={
-						<Text size='xs' transform='uppercase' weight={700} color='dimmed'>
-							Ctrl + U
-						</Text>
-					}
-				>
-					Team
-				</Menu.Item>
-				<Menu.Item
-					icon={<IconCalendar size={16} color={theme.colors.violet[6]} stroke={1.5} />}
-					rightSection={
-						<Text size='xs' transform='uppercase' weight={700} color='dimmed'>
-							Ctrl + E
-						</Text>
-					}
-				>
-					Event
-				</Menu.Item>
-			</Menu.Dropdown>
-		</Menu>
-	)
+	const { t } = useTranslation()
+	const iconRender = approvedButtonIcons[iconKey]
+
+	return <Button leftIcon={<Icon icon={iconRender.icon} />}>{t(iconRender.labelKey)}</Button>
+}
+
+type Props = {
+	iconKey: keyof typeof approvedButtonIcons
 }
