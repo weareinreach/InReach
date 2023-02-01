912643812c19460aa52dfdd466983ebcb52b4225
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-02-01 10:29:56 -0500

    add hooks/scripts

---
+++ lib/postinstall.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable turbo/no-undeclared-env-vars */
+/* eslint-disable import/no-unused-modules */
+/* eslint-disable node/no-process-env */
+// @ts-ignore
+const isCi = process.env.CI !== undefined
+if (!isCi) {
+	const { execSync } = require('child_process')
+	execSync('pnpm db:generate', { stdio: 'inherit' })
+}
