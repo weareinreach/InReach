95a5b0d90930b342f548b06f50466305f799578c
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-02-01 21:54:22 -0500

    IN-763 create audit log parser

--- packages/api/lib/auditLog.ts
+++ packages/api/lib/auditLog.ts
@@ -1,6 +1,30 @@
-import { Prisma } from '@weareinreach/db'
+import { Prisma, PrismaClient, prisma, AuditLog } from '@weareinreach/db'
+import { z } from 'zod'
 
-type ConnectUser = {
-	connect: Prisma.UserWhereUniqueInput
+import { InputJsonValueType } from 'schemas/common'
+
+import { PrismaTables, AuditLogSchema, prismaQueries, QueryParams } from '../schemas/auditLog'
+
+export const auditLog: AuditLogParser = async (table, method, data) => {
+	const parser = AuditLogSchema[table]
+
+	return parser.parse(data)
 }
-export const connectUser = (id: string): ConnectUser => ({ connect: { id } })
+
+type AuditLogParser = <T extends Readonly<PrismaTables>, M extends Method>(
+	table: T extends Readonly<PrismaTables> ? T : never,
+	method: M,
+	data: ZodData<T>
+) => Promise<AuditLogReturn<T>>
+
+type Method = CreateMethod | UpdateMethod
+
+type CreateMethod = 'create' | 'link'
+type UpdateMethod = 'update' | 'delete' | 'unlink'
+type ZodData<T extends Readonly<PrismaTables>> = z.input<(typeof AuditLogSchema)[T]>
+
+type AuditLogReturn<T extends Readonly<PrismaTables>> = z.output<(typeof AuditLogSchema)[T]>
+
+type DataProp<T extends Readonly<PrismaTables>, M extends Method> = M extends CreateMethod
+	? Omit<ZodData<T>, 'from'> & Partial<Pick<ZodData<T>, 'from'>>
+	: ZodData<T>
--- packages/api/schemas/common.ts
+++ packages/api/schemas/common.ts
@@ -1,3 +1,4 @@
+import { Prisma } from '@weareinreach/db'
 import { z } from 'zod'
 
 const cuid = z.string().cuid()
@@ -10,3 +11,16 @@ export const orgIdLocationId = orgId.extend({ locationId: cuid })
 export const slug = z.object({ slug: z.string() })
 
 export const searchTerm = z.object({ search: z.string() })
+
+export const InputJsonValue: z.ZodType<Prisma.InputJsonValue> = z.union([
+	z.string(),
+	z.number(),
+	z.boolean(),
+	z.lazy(() => z.array(InputJsonValue.nullable())),
+	z.lazy(() => z.record(InputJsonValue.nullable())),
+])
+export type InputJsonValueType = z.infer<typeof InputJsonValue>
+
+export const JsonNullValueInputSchema = z.enum(['JsonNull'])
+
+export const JsonInputOrNull = z.union([z.lazy(() => JsonNullValueInputSchema), InputJsonValue])
