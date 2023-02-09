a0d5fe07840050133f2769f52e5446b2a273dbe8
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-02-08 19:11:12 -0500

    IN-758 create orgLocation

--- packages/api/router/location.ts
+++ packages/api/router/location.ts
@@ -1,7 +1,8 @@
 import { TRPCError } from '@trpc/server'
 
-import { defineRouter, publicProcedure, handleError } from '~api/lib'
+import { defineRouter, publicProcedure, handleError, staffProcedure } from '~api/lib'
 import { id, orgId } from '~api/schemas/common'
+import { CreateManyOrgLocationSchema, CreateOrgLocationSchema } from '~api/schemas/create/orgLocation'
 import { orgLocationInclude } from '~api/schemas/selects/org'
 
 export const locationRouter = defineRouter({
@@ -31,4 +32,43 @@ export const locationRouter = defineRouter({
 			handleError(error)
 		}
 	}),
+	create: staffProcedure.input(CreateOrgLocationSchema().inputSchema).mutation(async ({ ctx, input }) => {
+		try {
+			const data = CreateOrgLocationSchema().dataParser.parse(input)
+
+			const result = await ctx.prisma.orgLocation.create(data)
+
+			return result
+		} catch (error) {
+			handleError(error)
+		}
+	}),
+	createMany: staffProcedure
+		.input(CreateManyOrgLocationSchema().inputSchema)
+		.mutation(async ({ ctx, input }) => {
+			try {
+				const records = CreateManyOrgLocationSchema().dataParser.parse({
+					actorId: ctx.session.user.id,
+					operation: 'CREATE',
+					data: input,
+				})
+				const result = await ctx.prisma.$transaction(async (tx) => {
+					const locations = await tx.orgLocation.createMany({
+						data: records.data,
+						skipDuplicates: true,
+					})
+					const auditLogs = await tx.auditLog.createMany({
+						data: records.auditLogs,
+						skipDuplicates: true,
+					})
+					return {
+						locations: locations.count,
+						auditLogs: auditLogs.count,
+					}
+				})
+				return result
+			} catch (error) {
+				handleError(error)
+			}
+		}),
 })
--- packages/api/schemas/create/orgLocation.ts
+++ packages/api/schemas/create/orgLocation.ts
@@ -1,7 +1,26 @@
-import { Prisma, GeoJSONPointSchema } from '@weareinreach/db'
+import { Prisma, GeoJSONPointSchema, createId } from '@weareinreach/db'
 import { z } from 'zod'
 
-export const CreateOrgLocationSchema = z.object({
+import { cuid, JsonInputOrNullSuperJSON, MutationBase, MutationBaseArray } from '~api/schemas/common'
+
+import { AuditLogSchema } from './auditLog'
+import { createMany } from './common'
+
+export const LinkOrgLocationServiceSchema = z.object({
+	orgLocationId: cuid,
+	serviceId: cuid,
+})
+export const LinkOrgLocationEmailSchema = z.object({
+	orgLocationId: cuid,
+	orgEmailId: cuid,
+})
+export const LinkOrgLocationPhoneSchema = z.object({
+	orgLocationId: cuid,
+	phoneId: cuid,
+})
+
+export const CreateOrgLocationBaseSchema = z.object({
+	id: cuid.optional(),
 	name: z.string().optional(),
 	street1: z.string(),
 	street2: z.string().optional(),
@@ -15,76 +34,106 @@ export const CreateOrgLocationSchema = z.object({
 	geoJSON: GeoJSONPointSchema,
 	published: z.boolean().default(false),
 })
+const OrgLocationLinksSchema = z.object({
+	emails: LinkOrgLocationEmailSchema.omit({ orgLocationId: true }).array().optional(),
+	phones: LinkOrgLocationPhoneSchema.omit({ orgLocationId: true }).array().optional(),
+	services: LinkOrgLocationServiceSchema.omit({ orgLocationId: true }).array().optional(),
+})
 
-export const CreateNestedOrgLocationSchema = CreateOrgLocationSchema.array().transform((data) =>
-	Prisma.validator<Prisma.OrgLocationCreateNestedManyWithoutOrganizationInput>()({
-		createMany: {
-			data: data.map(
-				({
-					name,
-					street1,
-					street2,
-					city,
-					postCode,
-					primary,
-					govDistId,
-					countryId,
-					longitude,
-					latitude,
-					geoJSON,
-					published,
-				}) => ({
-					name,
-					street1,
-					street2,
-					city,
-					postCode,
-					primary,
-					govDistId,
-					countryId,
-					longitude,
-					latitude,
-					geoJSON,
-					published,
-				})
-			),
-		},
-	})
+export const CreateNestedOrgLocationSchema = CreateOrgLocationBaseSchema.array().transform((data) =>
+	Prisma.validator<Prisma.Enumerable<Prisma.OrgLocationCreateManyOrganizationInput>>()(data)
 )
 
-// export const CreateNestedOrgLocationPrisma = (data?: z.infer<typeof CreateNestedOrgLocationSchema>) => {
-// 	if (!data) return undefined
-// 	return Prisma.validator<Prisma.OrgLocationCreateNestedManyWithoutOrganizationInput>()({
-// 		createMany: {
-// 			data: data.map(
-// 				({
-// 					name,
-// 					street1,
-// 					street2,
-// 					city,
-// 					postCode,
-// 					primary,
-// 					govDistId,
-// 					countryId,
-// 					longitude,
-// 					latitude,
-// 					geoJSON,
-// 					published,
-// 				}) => ({
-// 					name,
-// 					street1,
-// 					street2,
-// 					city,
-// 					postCode,
-// 					primary,
-// 					govDistId,
-// 					countryId,
-// 					longitude,
-// 					latitude,
-// 					geoJSON,
-// 					published,
-// 				})
-// 			),
-// 		},
-// 	})
-// }
+export const CreateManyOrgLocationSchema = () => {
+	const { dataParser: parser, inputSchema } = MutationBaseArray(
+		CreateOrgLocationBaseSchema.extend({ orgId: z.string() })
+	)
+	const dataParser = parser.transform(({ actorId, data, operation }) => {
+		const dataOut: Prisma.OrgLocationCreateManyInput[] = []
+		const auditLogs: Prisma.AuditLogCreateManyInput[] = []
+
+		for (const record of data) {
+			const id = createId()
+			dataOut.push(
+				Prisma.validator<Prisma.OrgLocationCreateManyInput>()({
+					id,
+					...record.to,
+				})
+			)
+			auditLogs.push(
+				Prisma.validator<Prisma.AuditLogCreateManyInput>()({
+					actorId,
+					operation,
+					from: JsonInputOrNullSuperJSON.parse(record.from),
+					to: JsonInputOrNullSuperJSON.parse(record.to),
+					orgLocationId: id,
+				})
+			)
+		}
+		return { data: dataOut, auditLogs }
+	})
+
+	return { dataParser, inputSchema }
+}
+
+export const CreateOrgLocationSchema = () => {
+	const { dataParser: parser, inputSchema } = MutationBase(
+		CreateOrgLocationBaseSchema.extend({ orgId: z.string() }).merge(OrgLocationLinksSchema)
+	)
+	const dataParser = parser.transform(({ actorId, operation, from, to }) => {
+		const { emails, phones, services, ...dataTo } = to
+		const linkAuditLogs: Prisma.AuditLogUncheckedCreateWithoutOrgLocationInput[] = []
+		const serviceLinks = (
+			!services
+				? undefined
+				: createMany(
+						services.map(({ serviceId }) => {
+							linkAuditLogs.push(AuditLogSchema.parse({ actorId, operation: 'LINK', serviceId }))
+							return { serviceId }
+						})
+				  )
+		) satisfies Prisma.OrgLocationServiceCreateNestedManyWithoutLocationInput | undefined
+
+		const emailLinks = (
+			!emails
+				? undefined
+				: createMany(
+						emails.map(({ orgEmailId }) => {
+							linkAuditLogs.push(AuditLogSchema.parse({ actorId, operation: 'LINK', orgEmailId }))
+							return { orgEmailId }
+						})
+				  )
+		) satisfies Prisma.OrgLocationEmailCreateNestedManyWithoutLocationInput | undefined
+		const phoneLinks = (
+			!phones
+				? undefined
+				: createMany(
+						phones.map(({ phoneId }) => {
+							linkAuditLogs.push(AuditLogSchema.parse({ actorId, operation: 'LINK', phoneId }))
+							return { phoneId }
+						})
+				  )
+		) satisfies Prisma.OrgLocationPhoneCreateNestedManyWithoutLocationInput | undefined
+		const auditEntry = AuditLogSchema.parse({
+			actorId,
+			operation: 'CREATE',
+			to: dataTo,
+		})
+
+		return Prisma.validator<Prisma.OrgLocationCreateArgs>()({
+			data: {
+				...dataTo,
+				emails: emailLinks,
+				phones: phoneLinks,
+				services: serviceLinks,
+				auditLogs: {
+					createMany: {
+						data: [auditEntry, ...linkAuditLogs],
+					},
+				},
+			},
+		})
+	})
+
+	return { dataParser, inputSchema }
+}
