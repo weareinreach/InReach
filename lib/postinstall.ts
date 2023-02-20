e3b1171d8d6227ad8bae33061221e09a28b77126
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-02-20 13:28:45 -0500

    CI updates

--- lib/postinstall.ts
+++ lib/postinstall.ts
@@ -5,7 +5,8 @@
 /* eslint-disable node/no-process-env */
 // @ts-ignore
 const isCi = process.env.CI !== undefined
-if (!isCi) {
+const override = process.env.OVERRIDE_CI !== undefined
+if (!isCi || override) {
 	const { execSync } = require('child_process')
 	execSync('turbo run post-install', { stdio: 'inherit' })
 }
