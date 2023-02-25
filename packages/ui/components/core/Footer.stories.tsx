24a4e44a38e7abe987156d8bc5eb742d0e310b79
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-02-25 01:11:59 -0400

    created .stories file for footer component

---
+++ packages/ui/components/core/Footer.stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Footer } from './Footer'
+
+export default {
+	title: 'Design System/Footer',
+	component: Footer,
+	parameters: {
+		layout: 'fullscreen',
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/gl8ppgnhpSq1Dr7Daohk55/Design-System-(2023)?t=cm6XAiXXhLOXJAQY-0',
+		},
+	},
+} satisfies Meta<typeof Footer>
+
+type StoryDef = StoryObj<typeof Footer>
+
+export const Default = {} satisfies StoryDef
