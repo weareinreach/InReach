1d9098d32f421655a02d7f6ca3730886f91fe649
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-03-02 22:19:05 -0400

    added figma link

--- packages/ui/components/sections/Navbar.stories.tsx
+++ packages/ui/components/sections/Navbar.stories.tsx
@@ -8,6 +8,10 @@ export default {
 	component: Navbar,
 	render: () => <Navbar />,
 	parameters: {
+		design: {
+			type: 'figma',
+			url: 'https://www.figma.com/file/gl8ppgnhpSq1Dr7Daohk55/Design-System-(2023)?node-id=68%3A389&t=QPSEMSrkiRBHDGDr-0',
+		},
 		layout: 'fullscreen',
 	},
 	//decorators: [StorybookGrid],
