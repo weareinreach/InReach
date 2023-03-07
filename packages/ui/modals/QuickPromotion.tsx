7648db26a7ef43cabf598a0c2923772ddaa7bc3a
trigal <10123216+trigal2012@users.noreply.github.com>
2023-03-06 20:24:45 -0800

    basic text and styles

--- packages/ui/modals/QuickPromotion.tsx
+++ packages/ui/modals/QuickPromotion.tsx
@@ -1,7 +1,8 @@
-import { createStyles, Group, List, Text } from '@mantine/core'
+import { createStyles, Group, List, Text, useMantineTheme, rem, em } from '@mantine/core'
 import { useMediaQuery, useViewportSize } from '@mantine/hooks'
 import { openContextModal } from '@mantine/modals'
 import { ContextModalProps } from '@mantine/modals/lib/context'
+import Link from 'next/link'
 import { signIn } from 'next-auth/react'
 import { useTranslation } from 'next-i18next'
 
@@ -11,43 +12,61 @@ import { ModalTitle, ModalTitleProps } from './ModalTitle'
 
 const useStyles = createStyles((theme) => ({
 	container: {
-		width: '600px',
 		flexDirection: 'column',
+		marginTop: rem(40),
 	},
-	heading1: {},
-	heading2: {},
-	body: {},
-	list: {},
+	heading1: {
+		...theme.other.utilityFonts.utility1,
+		fontSize: rem(40),
+	},
+	heading2: {
+		...theme.other.utilityFonts.utility1,
+		fontSize: rem(24),
+	},
+	heading3: {
+		...theme.other.utilityFonts.utility2,
+		color: theme.other.colors.secondary.darkGray,
+		marginBottom: rem(24),
+	},
+	body: {
+		...theme.other.utilityFonts.utility1,
+		color: theme.other.colors.secondary.darkGray,
+	},
+	button: {
+		marginTop: rem(24),
+		width: '95%',
+	},
+	link: {
+		marginTop: rem(34),
+		marginBottom: rem(50),
+	},
 }))
 
 export const QuickPromotionModalBody = ({ context, id, innerProps }: ContextModalProps<{}>) => {
-	const { t } = useTranslation(['common', 'services'])
-	const { classes, cx } = useStyles()
+	const { t } = useTranslation(['common'])
+	const { classes } = useStyles()
+	const theme = useMantineTheme()
 
 	return (
 		<Group position='center' className={classes.container}>
-			🌈
-			<Text>You need to log in to save resources.</Text>
-			<Text>With a free InReach account you can unlock additional features:</Text>
-			<List>
-				<ul>💚 Save and share personalized resource lists</ul>
-				<ul>💬 Leave public rating/reviews on organizations</ul>
-				<ul>🏠 Suggest organizations in your area</ul>
-				<ul>🔗 Claim your organization’s profile page</ul>
-			</List>
-			<Button>Log in</Button>
-			<a>Don’t have an account?</a>
+			<Text className={classes.heading1}>🌈</Text>
+			<Text className={classes.heading2}>You need to log in to save resources.</Text>
+			<Text className={classes.heading3}>
+				With a free InReach account you can unlock additional features:
+			</Text>
+			<Text className={classes.body}>💚 Save and share personalized resource lists</Text>
+			<Text className={classes.body}>💬 Leave public rating/reviews on organizations</Text>
+			<Text className={classes.body}>🏠 Suggest organizations in your area</Text>
+			<Text className={classes.body}>🔗 Claim your organization’s profile page</Text>
+			<Button className={classes.button}>Log in</Button>
+			<Link href='/' onClick={() => {}} onMouseEnter={() => {}} onTouchStart={() => {}}>
+				Don’t have an account?
+			</Link>
 		</Group>
 	)
 }
 
 const modalTitle = <ModalTitle breadcrumb={{ option: 'close' }} />
-
-// export const LoginModal = () =>
-// 	({
-// 		title: modalTitle,
-// 		children: <LoginModalBody />,
-// 	} satisfies ModalSettings)
 
 export const openQuickPromotionModal = () =>
 	openContextModal({
