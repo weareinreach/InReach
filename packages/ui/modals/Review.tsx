1b402be79ddb96733668aa793c065533b844060f
trigal <10123216+trigal2012@users.noreply.github.com>
2023-03-06 21:41:41 -0800

    remove extra stuff

--- packages/ui/modals/Review.tsx
+++ packages/ui/modals/Review.tsx
@@ -1,7 +1,6 @@
-import { Text, createStyles, rem } from '@mantine/core'
+import { createStyles } from '@mantine/core'
 import { openContextModal } from '@mantine/modals'
 import { ContextModalProps } from '@mantine/modals/lib/context'
-import { useTranslation } from 'next-i18next'
 
 import { ModalTitle, ModalTitleProps } from './ModalTitle'
 import { UserReviewSubmit } from '../components/core/UserReviewSubmit'
@@ -9,9 +8,6 @@ import { UserReviewSubmit } from '../components/core/UserReviewSubmit'
 const useStyles = createStyles((theme) => ({}))
 
 export const ReviewModalBody = ({ context, id, innerProps }: ContextModalProps<{}>) => {
-	const { t } = useTranslation(['common'])
-	const { classes } = useStyles()
-
 	return <UserReviewSubmit />
 }
 
