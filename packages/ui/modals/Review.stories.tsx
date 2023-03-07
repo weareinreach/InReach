3110f9ea3ce2421412f70b0caa21dcbb59608d5a
trigal <10123216+trigal2012@users.noreply.github.com>
2023-03-06 22:16:42 -0800

    adding mock stuff to story

--- packages/ui/modals/Review.stories.tsx
+++ packages/ui/modals/Review.stories.tsx
@@ -1,8 +1,10 @@
 import { Meta } from '@storybook/react'
 
 import { Button } from '~ui/components/core'
+import { getTRPCMock } from '~ui/lib/getTrpcMock'
 
 import { openReviewModal } from './Review'
+// import { UserReviewSubmit } from '../components/core/UserReviewSubmit'
 
 const ModalTemplate = () => {
 	return <Button onClick={openReviewModal}>Open Modal</Button>
@@ -11,6 +13,41 @@ const ModalTemplate = () => {
 export default {
 	title: 'Modals/Review',
 	component: ModalTemplate,
+	parameters: {
+		nextjs: {
+			router: {
+				pathname: '/org/[slug]',
+				asPath: '/org/mockOrg',
+				query: {
+					slug: 'mockOrg',
+				},
+			},
+		},
+		msw: {
+			handlers: [
+				getTRPCMock({
+					path: ['organization', 'getIdFromSlug'],
+					type: 'query',
+					response: {
+						id: 'orgn_ORGANIZATIONID',
+					},
+				}),
+				getTRPCMock({
+					path: ['review', 'create'],
+					type: 'mutation',
+					response: {
+						id: 'orev_NEWREVIEWID',
+					},
+				}),
+			],
+		},
+	},
 } satisfies Meta<typeof ModalTemplate>
 
-export const Modal = {}
+export const Modal = {
+	parameters: {
+		nextAuthMock: {
+			session: 'noAuth',
+		},
+	},
+}
