6e9aa392003c2cdd32463255525bc0c3c3b29f22
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-03-08 01:51:57 -0500

    contact section

--- packages/ui/components/sections/Contact.tsx
+++ packages/ui/components/sections/Contact.tsx
@@ -1,12 +1,174 @@
+import { Stack, Title, Text, Grid, Paper, useMantineTheme } from '@mantine/core'
+import { useMediaQuery, useViewportSize } from '@mantine/hooks'
 import { type ApiOutput } from '@weareinreach/api'
+import parsePhoneNumber, { type CountryCode } from 'libphonenumber-js'
+import { useRouter } from 'next/router'
+import { TFunction, useTranslation } from 'next-i18next'
+import { ReactNode } from 'react'
+import { z } from 'zod'
+
+import { Link, isExternal, SocialLink, SocialLinkProps } from '~ui/components/core'
+import { useCustomVariant, useScreenSize } from '~ui/hooks'
+
+const urlValid = (url: unknown): url is `http${string}` => {
+	const result = z.string().url().safeParse(url)
+	return result.success
+}
+
+const WebsiteList = ({ role, websites, t }: WebsiteListProps) => {
+	const variants = useCustomVariant()
+	const domainExtract = /https?:\/\/([^:\/\n?]+)/
+	const sites: JSX.Element[] = []
+	try {
+		for (const site of websites) {
+			const urlBase = site.url.match(domainExtract)
+			console.log(urlValid(site.url), urlBase)
+			if (!urlValid(site.url)) continue
+			if (!urlBase) continue
+			if (role === 'org' && (site.orgLocationOnly || site.orgLocationId)) continue
+			const addItem = (item: (typeof sites)[number]) =>
+				site.isPrimary && sites.length > 1 ? sites.unshift(item) : sites.push(item)
+
+			const description = `${
+				urlBase[1]
+				// site.description?.key
+				// 	? t(site.description.key, { defaultText: site.description.tsKey.text })
+				// 	: urlBase[1]
+			}`
+			const linkProps = {
+				key: site.id,
+				href: site.url,
+				external: true,
+				variant: variants.Link.inline,
+			}
+			const link = <Link {...linkProps}>{description}</Link>
+			addItem(link)
+		}
+		if (!sites.length) return <></>
+
+		return (
+			<Stack>
+				<Title order={3}>{t('website')}</Title>
+				{/* {sites.length &&
+					sites.map(({ description, key, ...props }) => (
+						<Link key={key} {...props} external variant={variants.Link.inline}>
+							{description}
+						</Link>
+					))} */}
+				{sites}
+			</Stack>
+		)
+	} catch (error) {
+		console.log(error)
+		if (error instanceof Error) console.error(error.message, error.cause)
+		return <>{JSON.stringify({ error, sites })}</>
+	}
+}
+const Item = ({ heading, href, subheading }: { heading: string; href: string; subheading?: string }) => {
+	const variants = useCustomVariant()
+	if (!isExternal(href)) return null
+	return (
+		<Stack spacing={4}>
+			<Link external href={href} variant={variants.Link.inlineInverted}>
+				{heading}
+			</Link>
+			{Boolean(subheading) && <Text variant={variants.Text.utility4darkGray}>{subheading}</Text>}
+		</Stack>
+	)
+}
+
+const ItemListWithSubtext = (props: SubtextList) => {
+	const { role, dataType, data, t } = props
+	const items: ReactNode[] = []
+	if (!data.length) return null
+
+	const isEmail = (dataType: string, data: unknown): data is EmailList['data'] => dataType === 'email'
+
+	switch (dataType) {
+		case 'phone': {
+			for (const { phone } of data) {
+				const addItem = (item: ReactNode) => (phone.primary ? items.unshift(item) : items.push(item))
+				const parsedPhone = parsePhoneNumber(phone.number, phone.country.cca2 as CountryCode)
+				if (!parsedPhone) continue
+				const phoneNumber = parsedPhone.formatNational()
+				const desc = phone.phoneType?.tsKey ? t(phone.phoneType.tsKey) : undefined
+				const href = parsedPhone.getURI()
+				const itemProps = { heading: phoneNumber, href, subheading: desc }
+				const item = <Item key={phone.number} {...itemProps} />
+				console.log('phone', item)
+				addItem(item)
+			}
+			break
+		}
+		case 'email':
+			{
+				if (!isEmail(dataType, data)) return null
+				for (const { email } of data) {
+					const addItem = (item: ReactNode) => (email.primary ? items.unshift(item) : items.push(item))
+
+					const desc = email.title
+						? t(email.title.tsKey)
+						: email.description?.key
+						? t(email.description.key, { defaultText: email.description.tsKey.text })
+						: undefined
+					const href = `mailto:${email.email}`
+
+					const itemProps = { heading: email.email, href, subheading: desc }
+
+					items.push(<Item key={email.email} {...itemProps} />)
+				}
+			}
+			break
+	}
+	console.log(dataType, items)
+	if (!items) return null
+	return (
+		<Stack spacing={12}>
+			<Title order={3}>{t(dataType)}</Title>
+			{...items}
+		</Stack>
+	)
+}
 
 export const ContactSection = (props: ContactSectionProps) => {
-	return <></>
+	const router = useRouter<'/org/[slug]' | '/org/[slug]/[orgLocationId]'>()
+	const { slug } = router.query
+	const { t } = useTranslation(['common', slug])
+	const { role } = props
+	const theme = useMantineTheme()
+	const { isMobile } = useScreenSize()
+	const { emails, phones, socialMedia, websites } = props.data
+	const hasData = (dataType: (typeof props.data)[keyof typeof props.data]) => Boolean(dataType.length > 0)
+
+	const socialLinks: SocialLinkProps[] = socialMedia.map((item) => ({
+		icon: item.service.name.toLowerCase() as SocialLinkProps['icon'],
+		href: item.url,
+		title: item.username,
+	}))
+	const body = (
+		<Stack spacing={isMobile ? 32 : 40}>
+			<Title order={2}>{t('contact')}</Title>
+			<WebsiteList role={role} websites={websites} t={t} />
+			<ItemListWithSubtext role={role} dataType='phone' data={phones} t={t} />
+			<ItemListWithSubtext role={role} dataType='email' data={emails} t={t} />
+			<SocialLink.Group links={socialLinks} header />
+		</Stack>
+	)
+	console.log(isMobile, useViewportSize())
+	if (isMobile) return <Grid.Col>{body}</Grid.Col>
+	return (
+		<Grid.Col>
+			<Paper withBorder>{body}</Paper>
+		</Grid.Col>
+	)
 }
 
 type PageQueryResult = NonNullable<ApiOutput['organization']['getBySlug']>
 
-export type ContactSectionProps = {
+export type ContactSectionProps = OrganizationContact | LocationContact
+
+type OrganizationContact = {
+	role: 'org'
 	data: {
 		websites: PageQueryResult['websites']
 		phones: PageQueryResult['phones']
@@ -14,3 +176,44 @@ export type ContactSectionProps = {
 		socialMedia: PageQueryResult['socialMedia']
 	}
 }
+type Location = NonNullable<PageQueryResult['locations']>[number]
+
+type LocationContact = {
+	role: 'location'
+	data: {
+		websites: Location['websites']
+		phones: Location['phones']
+		emails: Location['emails']
+		socialMedia: Location['socialMedia']
+	}
+}
+type SectionRole = LocationContact['role'] | OrganizationContact['role']
+
+type LocationData = NonNullable<LocationContact['data'] | OrganizationContact['data']>
+
+type WebsiteListProps = {
+	role: SectionRole
+	websites: LocationData['websites']
+	t: TFunction
+}
+
+type WebsiteList = {
+	role: SectionRole
+	dataType: 'website'
+	websites: LocationData['websites']
+	t: TFunction
+}
+
+type PhoneList = {
+	role: SectionRole
+	dataType: 'phone'
+	data: PageQueryResult['phones'] | Location['phones']
+	t: TFunction
+}
+type EmailList = {
+	role: SectionRole
+	dataType: 'email'
+	data: PageQueryResult['emails'] | Location['emails']
+	t: TFunction
+}
+type SubtextList = PhoneList | EmailList
