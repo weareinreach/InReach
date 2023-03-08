6852f9700106c01d834357adbd815bf0b57aed85
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-03-08 18:26:36 -0500

    init

--- packages/ui/components/sections/VisitCard.tsx
+++ packages/ui/components/sections/VisitCard.tsx
@@ -1,6 +1,15 @@
+import { Title, Grid, Card, List, Stack, Group } from '@mantine/core'
 import { type ApiOutput } from '@weareinreach/api'
+import { formatAddress } from 'localized-address-format'
+import { useTranslation } from 'next-i18next'
+
+import { useCustomVariant, useScreenSize } from '~ui/hooks'
+import { type IconList } from '~ui/icon'
 
 export const VisitCard = (props: VisitCardProps) => {
+	const variants = useCustomVariant()
+	const { isMobile } = useScreenSize()
+
 	return <></>
 }
 
