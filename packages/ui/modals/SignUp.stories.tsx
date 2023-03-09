ea850860d31d79d6879d292f08f5ee79c22a1200
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-03-09 18:37:11 -0500

    init

--- packages/ui/modals/SignUp.stories.tsx
+++ packages/ui/modals/SignUp.stories.tsx
@@ -1,3 +1,4 @@
+import { Center } from '@mantine/core'
 import { Meta } from '@storybook/react'
 
 import { Button } from '~ui/components/core'
@@ -8,12 +9,19 @@ import { openLoginModal } from './Login'
 // const modalToDisplay = LoginModal()
 
 const ModalTemplate = () => {
-	return <Button onClick={openLoginModal}>Open Modal</Button>
+	return (
+		<Center maw='100vw' h='100vh'>
+			<Button onClick={openLoginModal}>Open Modal</Button>
+		</Center>
+	)
 }
 
 export default {
 	title: 'Modals/Sign Up',
 	component: ModalTemplate,
+	parameters: {
+		layout: 'fullscreen',
+	},
 } satisfies Meta<typeof ModalTemplate>
 
 export const Modal = {}
