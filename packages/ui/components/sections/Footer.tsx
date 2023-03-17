b7dbdc52aceeee2cbd50e808cb0fe9b247ff15db
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-03-17 12:54:17 -0400

    update links

--- packages/ui/components/sections/Footer.tsx
+++ packages/ui/components/sections/Footer.tsx
@@ -1,13 +1,15 @@
 import { Title, Grid, Text, Stack, Group, createStyles, rem } from '@mantine/core'
 import Image from 'next/image'
-import { useTranslation } from 'next-i18next'
+import { useTranslation, Trans } from 'next-i18next'
+import { MouseEventHandler } from 'react'
 
 import InReach from '~ui/assets/inreach.svg'
 import Vercel from '~ui/assets/vercel.svg'
-import { Link } from '~ui/components/core/Link'
+import { Link, type InternalLink, type ExternalLink } from '~ui/components/core/Link'
 import { SocialLink } from '~ui/components/core/SocialLink'
 import { useCustomVariant } from '~ui/hooks'
 import { BodyGrid } from '~ui/layouts'
+import { GenericContentModal, PrivacyStatementModal } from '~ui/modals'
 
 const useStyles = createStyles((theme) => ({
 	link: {
@@ -29,42 +31,60 @@ const useStyles = createStyles((theme) => ({
 	},
 }))
 
-const supportLinks = [
-	['footer-suggest-org', '#'],
-	['footer-share-feedback', '#'],
-	['footer-vetting-process', '#'],
-	['footer-privacy-statement', '#'],
-	['footer-anti-hate', '#'],
-	['footer-digital-accessibility', '#'],
-	['footer-disclaimer', '#'],
-]
+type FooterLinkInt = {
+	key: string
+	href: InternalLink
+	onClick?: never
+	external: false
+}
+type FooterLinkExt = {
+	key: string
+	href?: ExternalLink
+	onClick?: MouseEventHandler<HTMLAnchorElement>
+	external: true
+}
+type LinkArr = (FooterLinkInt | FooterLinkExt)[]
 
-const connectLinks = [
-	['inreach-org', '#'],
-	['download-app', '#'],
-	['footer-subscribe-newsletter', '#'],
+const connectLinks: LinkArr = [
+	{ key: 'inreach-org', href: 'https://www.inreach.org', external: true },
+	{ key: 'download-app', href: 'https://inreach.org/mobile-app/', external: true },
+	{ key: 'footer.subscribe-newsletter', href: 'https://inreach.org/newsletter/', external: true },
 ]
 
 export const Footer = () => {
 	const { t } = useTranslation('common')
 	const { classes, cx } = useStyles()
 	const variants = useCustomVariant()
-	const makeLinks = (links: string[][]) =>
-		links.map(([text, href]) => (
-			<Link
-				key={text}
-				external
-				href={`/${href}`}
-				variant={variants.Link.inlineInvertedUtil1}
-				className={classes.link}
-			>
-				{t(text as string)}
-			</Link>
-		))
+	const linkVar = { variant: variants.Link.inlineInvertedUtil1 }
+	const support = [
+		<Link key={0} href='/suggest' {...linkVar}>
+			{t('footer.suggest-org')}
+		</Link>,
+		<Link key={1} href='https://www.surveymonkey.com/r/96QD8ZQ' external {...linkVar}>
+			{t('footer.share-feedback')}
+		</Link>,
+		<Link key={2} href='https://inreach.org/vetting-process/' external {...linkVar}>
+			{t('footer.vetting-process')}
+		</Link>,
+		<PrivacyStatementModal key={3} component={Link} {...linkVar}>
+			{t('footer.privacy-statement')}
+		</PrivacyStatementModal>,
+		<GenericContentModal key={4} content='antiHate' component={Link} {...linkVar}>
+			{t('footer.anti-hate')}
+		</GenericContentModal>,
+		<GenericContentModal key={5} content='accessibilityStatement' component={Link} {...linkVar}>
+			{t('footer.digital-accessibility')}
+		</GenericContentModal>,
+		<GenericContentModal key={6} content='disclaimer' component={Link} {...linkVar}>
+			{t('footer.disclaimer')}
+		</GenericContentModal>,
+	]
 
-	const support = makeLinks(supportLinks)
-
-	const connect = makeLinks(connectLinks)
+	const connect = connectLinks.map(({ key, ...props }, idx) => (
+		<Link key={idx} variant={variants.Link.inlineInvertedUtil1} {...props}>
+			{t(key)}
+		</Link>
+	))
 
 	return (
 		<div className={classes.background}>
@@ -80,7 +100,8 @@ export const Footer = () => {
 								style={{ marginBottom: 0 }}
 							/>
 							<Title order={2} fw={500}>
-								{t('inreach-tagline')}
+								{/* {t('footer.tagline')} */}
+								<Trans i18nKey='footer.tagline' />
 							</Title>
 							<a href='https://vercel.com/?utm_source=in-reach&utm_campaign=oss' style={{ margin: 0 }}>
 								<Image
@@ -108,16 +129,24 @@ export const Footer = () => {
 						<Text fw={600}>{t('connect')}</Text>
 						{connect}
 						<Group noWrap className={classes.iconGroup}>
-							<SocialLink icon='facebook' href='#' title='Facebook' />
-							<SocialLink icon='twitter' href='#' title='Twitter' />
-							<SocialLink icon='linkedin' href='#' title='LinkedIn' />
-							<SocialLink icon='instagram' href='#' title='Instagram' />
+							<SocialLink icon='facebook' href='https://www.facebook.com/weareinreach' title='Facebook' />
+							<SocialLink icon='twitter' href='https://twitter.com/weareinreach' title='Twitter' />
+							<SocialLink
+								icon='linkedin'
+								href='https://www.linkedin.com/company/weareinreach'
+								title='LinkedIn'
+							/>
+							<SocialLink icon='instagram' href='https://www.instagram.com/weareinreach' title='Instagram' />
 						</Group>
 						<Group noWrap className={classes.iconGroup} pb='24px'>
-							<SocialLink icon='youtube' href='#' title='Youtube' />
-							<SocialLink icon='tiktok' href='#' title='TikTok' />
-							<SocialLink icon='github' href='#' title='GitHub' />
-							<SocialLink icon='mail' href='#' title={t('email')} />
+							<SocialLink
+								icon='youtube'
+								href='https://www.youtube.com/channel/UCJsVS5-0ymo40mRjCe4BIHA'
+								title='YouTube'
+							/>
+							<SocialLink icon='tiktok' href='https://www.tiktok.com/@weareinreach' title='TikTok' />
+							<SocialLink icon='github' href='https://github.com/weareinreach' title='GitHub' />
+							<SocialLink icon='mail' href='https://inreach.org/contact/' title={t('email')} />
 						</Group>
 					</Stack>
 				</Grid.Col>
