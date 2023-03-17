fc68f51c080fa31b0bd92bbdbf74f6a08707993b
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-03-17 12:50:23 -0400

    added variant without card wrapper

--- packages/ui/components/core/AntiHateMessage.tsx
+++ packages/ui/components/core/AntiHateMessage.tsx
@@ -1,4 +1,4 @@
-import { Card, Text, createStyles, Title } from '@mantine/core'
+import { Card, Text, createStyles, Title, Stack } from '@mantine/core'
 import { useTranslation } from 'next-i18next'
 
 const useStyles = createStyles((theme) => ({
@@ -7,16 +7,46 @@ const useStyles = createStyles((theme) => ({
 	},
 }))
 
-export const AntiHateMessage = () => {
+export const AntiHateMessage = ({ noCard, stacked }: AntiHateMessageProps) => {
 	const { classes } = useStyles()
 	const { t } = useTranslation()
 
-	return (
+	const title = stacked ? (
+		<>
+			<Title order={1}>🏳️‍🌈</Title>
+			<Title order={2}>{t('anti-hate.title')}</Title>
+		</>
+	) : (
+		<Title order={3}>{`🏳️‍🌈 ${t('anti-hate.title')}`}</Title>
+	)
+	const body = (
+		<Text className={classes.text} component='p' ta={stacked ? 'center' : undefined}>
+			{t('anti-hate.body')}
+		</Text>
+	)
+
+	const content = stacked ? (
+		<Stack spacing={16} align='center'>
+			{title}
+			{body}
+		</Stack>
+	) : (
+		<>
+			{title}
+			{body}
+		</>
+	)
+
+	return noCard ? (
+		content
+	) : (
 		<Card radius='lg' withBorder p={20}>
-			<Title order={3}>{t('anti-hate-title')}</Title>
-			<Text className={classes.text} component='p'>
-				{t('anti-hate-text')}
-			</Text>
+			{content}
 		</Card>
 	)
+}
+
+export interface AntiHateMessageProps {
+	noCard?: boolean
+	stacked?: boolean
 }
