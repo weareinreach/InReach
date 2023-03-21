8a03d7e49071fe2ed88275dca26f0b7e77c55fad
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-03-20 20:54:02 -0400

    update generated files, i18n strings, add crowdin fields

--- packages/api/generated/userRole.ts
+++ packages/api/generated/userRole.ts
@@ -9,13 +9,13 @@ export const userRoles = [
 ] as const
 
 export const userRolesWithId = [
-	{ id: 'urle_00000000003R7BSS9MVC1C6Y4G', tag: 'resource-seeker' },
-	{ id: 'urle_0000000000KY1B1T0N3MPGS7NF', tag: 'service-provider' },
-	{ id: 'urle_000000000057NV51DKC3MEKPM1', tag: 'local-community-reviewer' },
-	{ id: 'urle_000000000083VVFN6D9MW7NY3F', tag: 'data-manager' },
-	{ id: 'urle_00000000006PWQ4KYVYYGK1DMS', tag: 'data-administrator' },
-	{ id: 'urle_0000000000EHEREQADT1M73JT6', tag: 'system-administrator' },
-	{ id: 'urle_00000000009DJJ8DTRAHSR0DF6', tag: 'system-user' },
+	{ id: 'urle_000000000088C9GAE1NR7S3YBF', tag: 'resource-seeker' },
+	{ id: 'urle_0000000000Q8JTPXWXKCK450GW', tag: 'service-provider' },
+	{ id: 'urle_0000000000FW2DGSC961J8A0VV', tag: 'local-community-reviewer' },
+	{ id: 'urle_0000000000TMZTKWF41JSNQG33', tag: 'data-manager' },
+	{ id: 'urle_00000000004P9WZGG6WEBVHQZ9', tag: 'data-administrator' },
+	{ id: 'urle_0000000000JTBA2NMF19AQ9C6X', tag: 'system-administrator' },
+	{ id: 'urle_00000000002AS12M4EDTPXNPEH', tag: 'system-user' },
 ] as const
 
 export type UserRoleTags = (typeof userRoles)[number]
---
+++ packages/db/prisma/migrations/20230321004941_crowdin_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "TranslationKey" ADD COLUMN     "crowdinId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "TranslationNamespace" ADD COLUMN     "crowdinId" INTEGER;
