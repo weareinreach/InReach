a172c57dad019559b1291c379af809ded15b3870
trigal <10123216+trigal2012@users.noreply.github.com>
2023-03-23 13:25:18 -0700

    forgot password modal updated to polymorphic component

--- packages/ui/modals/ForgotPassword.tsx
+++ packages/ui/modals/ForgotPassword.tsx
@@ -1,9 +1,22 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Stack, Text, TextInput, Title, useMantineTheme, Group } from '@mantine/core'
+import {
+	Stack,
+	Text,
+	TextInput,
+	Title,
+	useMantineTheme,
+	Group,
+	type ButtonProps,
+	Modal,
+	Box,
+	createPolymorphicComponent,
+} from '@mantine/core'
 import { useForm, zodResolver } from '@mantine/form'
+import { useDisclosure } from '@mantine/hooks'
 import { openContextModal } from '@mantine/modals'
 import { ContextModalProps } from '@mantine/modals/lib/context'
 import { useTranslation } from 'next-i18next'
+import { forwardRef } from 'react'
 import { z } from 'zod'
 
 import { Button } from '~ui/components/core'
@@ -11,66 +24,72 @@ import { useCustomVariant } from '~ui/hooks'
 import { Icon } from '~ui/icon'
 import { trpc as api } from '~ui/lib/trpcClient'
 
-import { ModalTitle, ModalTitleProps } from './ModalTitle'
+import { ModalTitle } from './ModalTitle'
 
-export const ForgotPasswordModalBody = ({ context, id, innerProps }: ContextModalProps<{}>) => {
-	const { t } = useTranslation(['common'])
-	const EmailSchema = z.object({
-		email: z.string().email({ message: t('error-enter-valid-email') as string }),
-	})
-	const form = useForm<FormProps>({
-		validate: zodResolver(EmailSchema),
-		validateInputOnBlur: true,
-	})
-	const variants = useCustomVariant()
-	const theme = useMantineTheme()
-	const pwResetHandler = api.user.resetPassword.useMutation()
+export const ForgotPasswordModalBody = forwardRef<HTMLButtonElement, ForgotPasswordModalProps>(
+	(props, ref) => {
+		const { t } = useTranslation(['common'])
+		const EmailSchema = z.object({
+			email: z.string().email({ message: t('error-enter-valid-email') as string }),
+		})
+		const form = useForm<FormProps>({
+			validate: zodResolver(EmailSchema),
+			validateInputOnBlur: true,
+		})
+		const variants = useCustomVariant()
+		const theme = useMantineTheme()
+		const pwResetHandler = api.user.resetPassword.useMutation()
 
-	const successMessage = (
-		<Group>
-			<Icon icon='carbon:checkmark-filled' color={theme.other.colors.primary.allyGreen} height={14} />
-			<Text variant={variants.Text.utility3}>{t('email-sent')}</Text>
-		</Group>
-	)
-	console.log('valid?', form.isValid())
-	// TODO: Add 'shake' animation on invalid submit attempt
-	return (
-		<Stack align='center' spacing={24}>
-			<Title order={2}>{t('reset-password')}</Title>
-			<Text variant={variants.Text.utility4darkGray}>{t('reset-password-message')}</Text>
-			<TextInput
-				label={t('email')}
-				placeholder={t('enter-email-placeholder') as string}
-				required
-				{...form.getInputProps('email')}
-				description={pwResetHandler.isSuccess ? successMessage : undefined}
-			/>
-			<Button
-				onClick={() => pwResetHandler.mutate(form.values)}
-				variant='primary-icon'
-				fullWidth
-				loaderPosition='center'
-				loading={pwResetHandler.isLoading}
-				disabled={!form.isValid()}
-			>
-				{t('send-email')}
-			</Button>
-		</Stack>
-	)
-}
+		const [opened, handler] = useDisclosure(false)
 
-const modalTitle = <ModalTitle breadcrumb={{ option: 'close' }} />
+		const modalTitle = <ModalTitle breadcrumb={{ option: 'close', onClick: () => handler.close() }} />
 
-export const openForgotPasswordModal = () =>
-	openContextModal({
-		modal: 'forgotPassword',
-		title: modalTitle,
-		innerProps: {},
-	})
+		const successMessage = (
+			<Group>
+				<Icon icon='carbon:checkmark-filled' color={theme.other.colors.primary.allyGreen} height={14} />
+				<Text variant={variants.Text.utility3}>{t('email-sent')}</Text>
+			</Group>
+		)
+		console.log('valid?', form.isValid())
+		// TODO: Add 'shake' animation on invalid submit attempt
+		return (
+			<>
+				<Modal title={modalTitle} opened={opened} onClose={() => handler.close()}>
+					<Stack align='center' spacing={24}>
+						<Title order={2}>{t('reset-password')}</Title>
+						<Text variant={variants.Text.utility4darkGray}>{t('reset-password-message')}</Text>
+						<TextInput
+							label={t('email')}
+							placeholder={t('enter-email-placeholder') as string}
+							required
+							{...form.getInputProps('email')}
+							description={pwResetHandler.isSuccess ? successMessage : undefined}
+						/>
+						<Button
+							onClick={() => pwResetHandler.mutate(form.values)}
+							variant='primary-icon'
+							fullWidth
+							loaderPosition='center'
+							loading={pwResetHandler.isLoading}
+							disabled={!form.isValid()}
+						>
+							{t('send-email')}
+						</Button>
+					</Stack>
+				</Modal>
+				<Box component='button' ref={ref} onClick={() => handler.open()} {...props} />
+			</>
+		)
+	}
+)
 
-type ForgotPasswordModalProps = {
-	title: ModalTitleProps
-}
+ForgotPasswordModalBody.displayName = 'ForgotPasswordModal'
+
+export const ForgotPasswordModal = createPolymorphicComponent<'button', ForgotPasswordModalProps>(
+	ForgotPasswordModalBody
+)
+
+export interface ForgotPasswordModalProps extends ButtonProps {}
 
 type FormProps = {
 	email: string
