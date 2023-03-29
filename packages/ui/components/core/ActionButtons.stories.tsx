07c28558096d48a2de3f5574875a1bab86bb6ac1
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-03-28 23:09:51 -0400

    updated story to use trpc mock and nextAuth mock

--- packages/ui/components/core/ActionButtons.stories.tsx
+++ packages/ui/components/core/ActionButtons.stories.tsx
@@ -1,5 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
 
+import { getTRPCMock } from '~ui/lib/getTrpcMock'
+
 import { ActionButtons as ActionButtonsComponent } from './ActionButtons'
 
 export default {
@@ -10,16 +12,67 @@ export default {
 			type: 'figma',
 			url: 'https://www.figma.com/file/gl8ppgnhpSq1Dr7Daohk55/Design-System-(2023)?node-id=52%3A1420&t=sleVeGl2lJv7Df18-4',
 		},
+		layout: 'fullscreen',
+		layoutWrapper: 'centeredHalf',
+		nextjs: {
+			router: {
+				pathname: '/org/[slug]',
+				asPath: '/org/mockOrg',
+				query: {
+					slug: 'mockOrg',
+				},
+			},
+		},
+		msw: {
+			handlers: [
+				getTRPCMock({
+					path: ['organization', 'getIdFromSlug'],
+					type: 'query',
+					response: {
+						id: 'orgn_ORGANIZATIONID',
+					},
+				}),
+				getTRPCMock({
+					path: ['review', 'create'],
+					type: 'mutation',
+					response: {
+						id: 'orev_NEWREVIEWID',
+					},
+				}),
+			],
+		},
 	},
+	render: ({ iconKey }) => (
+		<div>
+			<ActionButtonsComponent iconKey={iconKey} />
+		</div>
+	),
 } satisfies Meta<typeof ActionButtonsComponent>
 
 type StoryDef = StoryObj<typeof ActionButtonsComponent>
 
 export const Save = {
+	parameters: {
+		nextAuthMock: {
+			session: 'userPic',
+		},
+	},
+	args: {
+		iconKey: 'save',
+	},
+}
+
+export const SaveLoggedOut = {
+	parameters: {
+		nextAuthMock: {
+			session: 'noAuth',
+		},
+	},
 	args: {
 		iconKey: 'save',
 	},
 } satisfies StoryDef
+
 export const Saved = {
 	args: {
 		iconKey: 'saved',
@@ -46,6 +99,11 @@ export const Review = {
 	},
 } satisfies StoryDef
 export const More = {
+	parameters: {
+		nextAuthMock: {
+			session: 'userPic',
+		},
+	},
 	args: {
 		iconKey: 'more',
 	},
