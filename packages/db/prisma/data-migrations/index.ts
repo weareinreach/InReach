235707468ad5241bd6e5f073c11b6b3c582620c5
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-04-04 19:16:33 -0400

    data migration: org leader & focus

--- packages/db/prisma/data-migrations/index.ts
+++ packages/db/prisma/data-migrations/index.ts
@@ -1,4 +1,5 @@
-// codegen:start {preset: barrel, include: ./*.ts}
+// codegen:start {preset: barrel, include: ./**/*.ts}
 export * from './2023-03-29'
 export * from './2023-03-31'
+export * from './2023-04-04/index'
 // codegen:end
--- packages/db/prisma/dataMigrationRunner.ts
+++ packages/db/prisma/dataMigrationRunner.ts
@@ -1,6 +1,6 @@
 import { Listr, ListrTask as ListrTaskObj, ListrDefaultRenderer, ListrTaskWrapper } from 'listr2'
 
-import { job20220329, job20220330 } from './data-migrations'
+import { job20220329, job20220330, job20220404 } from './data-migrations'
 
 const renderOptions = {
 	bottomBar: 10,
@@ -9,17 +9,21 @@ const renderOptions = {
 } satisfies ListrJob['options']
 const injectOptions = (job: ListrJob) => ({ ...job, renderOptions })
 
-const jobs = new Listr<Context>([injectOptions(job20220329), injectOptions(job20220330)], {
-	rendererOptions: {
-		collapse: false,
-		showTimer: true,
-		collapseErrors: false,
-		formatOutput: 'wrap',
-	},
-	nonTTYRendererOptions: {
-		useIcons: true,
-	},
-})
+const jobs = new Listr<Context>(
+	[injectOptions(job20220329), injectOptions(job20220330), injectOptions(job20220404)],
+	{
+		rendererOptions: {
+			collapse: false,
+			collapseErrors: false,
+			collapseSkips: false,
+			showTimer: true,
+			formatOutput: 'wrap',
+		},
+		nonTTYRendererOptions: {
+			useIcons: true,
+		},
+	}
+)
 
 jobs.run()
 
