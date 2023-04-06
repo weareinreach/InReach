ba146f9edb6583cafe1cd1e63e1d8652124dfb09
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-04-06 00:59:34 -0400

    attribute supplement schemas for access instructions

--- packages/api/schemas/attributeSupplement.ts
+++ packages/api/schemas/attributeSupplement.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod'
 
+import { accessInstructions } from './attributeSupplements'
+
 const numMinMax = z.object({
 	/** Minumum of range */
 	min: z.number(),
@@ -24,11 +26,11 @@ const incompatible = z
 		/** Incompatible data from migration - needs to be cleaned up. */
 	})
 	.catchall(z.record(z.any()))
-const accessInstructions = z.object({
-	access_value: z.string().nullish(),
-	access_type: z.enum(['email', 'file', 'link', 'location', 'other', 'phone', '', 'publicTransit']),
-	instructions: z.string(),
-})
+// const accessInstructions = z.object({
+// 	access_value: z.string().nullish(),
+// 	access_type: z.enum(['email', 'file', 'link', 'location', 'other', 'phone', '', 'publicTransit']),
+// 	instructions: z.string(),
+// })
 const cost = z.object({
 	cost: z.number(),
 	currency: z.string().nullish(),
