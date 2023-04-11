b9ed6c68c917bd9c2a905e13e18a343184eb43bc
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-04-11 19:14:27 -0400

    IN-845 IN-846 Org attribute linking

--- packages/api/router/organization/mutations.ts
+++ packages/api/router/organization/mutations.ts
@@ -5,6 +5,7 @@ import {
 	CreateQuickOrgSchema,
 	CreateOrgSuggestionSchema,
 	type CreateOrgSuggestionInput,
+	AttachAttribute,
 } from '~api/schemas/create/organization'
 
 export const mutations = defineRouter({
@@ -44,5 +45,29 @@ export const mutations = defineRouter({
 			} catch (error) {
 				handleError(error)
 			}
+		}),
+	attachAttribute: permissionedProcedure
+		.meta({ hasPerm: ['editTeamOrg', 'editAnyOrg', 'editSingleOrg'] })
+		.input(AttachAttribute().inputSchema)
+		.mutation(async ({ ctx, input }) => {
+			const inputData = { actorId: ctx.session.user.id, operation: 'LINK', data: input }
+			const { translationKey, freeText, attributeSupplement, organizationAttribute, auditLogs } =
+				AttachAttribute().dataParser.parse(inputData)
+
+			const result = await ctx.prisma.$transaction(async (tx) => {
+				const tKey = translationKey ? tx.translationKey.create(translationKey) : undefined
+				const fText = freeText ? tx.freeText.create(freeText) : undefined
+				const aSupp = attributeSupplement ? tx.attributeSupplement.create(attributeSupplement) : undefined
+				const attrLink = tx.organizationAttribute.create(organizationAttribute)
+				const logs = tx.auditLog.createMany({ data: auditLogs, skipDuplicates: true })
+				return {
+					translationKey: tKey,
+					freeText: fText,
+					attributeSupplement: aSupp,
+					organizationAttribute: attrLink,
+					auditLog: logs,
+				}
+			})
+			return result
 		}),
 })
