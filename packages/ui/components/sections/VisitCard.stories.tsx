de9b0d371a312a1073975e30f62aaccbd5f65c0f
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-04-13 00:33:14 -0400

    added stories to test remote/unpublished variants

--- packages/ui/components/sections/VisitCard.stories.tsx
+++ packages/ui/components/sections/VisitCard.stories.tsx
@@ -5,11 +5,31 @@ import { locationMock } from '~ui/mockData/locationCard'
 
 import { VisitCard } from './VisitCard'
 
+const remote = {
+	attribute: {
+		tsKey: 'additional.offers-remote-services',
+		tsNs: 'attribute',
+		icon: null,
+		iconBg: null,
+		showOnLocation: null,
+		categories: [
+			{
+				category: {
+					tag: 'additional-information',
+					icon: null,
+				},
+			},
+		],
+	},
+	supplement: [],
+}
+
 export default {
 	title: 'Sections/Visit Info',
 	component: VisitCard,
 	args: {
 		location: locationMock,
+		published: true,
 	},
 	decorators: [StorybookGridSingle],
 	parameters: {
@@ -24,6 +44,26 @@ export const Mobile = {
 	parameters: {
 		viewport: {
 			defaultViewport: 'iphonex',
+		},
+	},
+} satisfies StoryDef
+
+export const Remote = {
+	args: {
+		location: {
+			...locationMock,
+			country: {},
+			govDist: {},
+			attributes: [remote],
+		},
+	},
+}
+
+export const RemoteWithPhysicalAddress = {
+	args: {
+		location: {
+			...locationMock,
+			attributes: [remote],
 		},
 	},
 } satisfies StoryDef
