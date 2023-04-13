ed0a718281e9e0265260669b2de605c6995ec672
Joaquinb2000 <joaquinberon2000@gmail.com>
2023-04-13 00:32:09 -0400

    added variations for remote/unpublished

--- packages/ui/components/sections/VisitCard.tsx
+++ packages/ui/components/sections/VisitCard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
 /* eslint-disable node/no-process-env */
-import { Title, Card, Stack, Text } from '@mantine/core'
+import { Title, Card, Stack, Group, Text } from '@mantine/core'
 import { useElementSize } from '@mantine/hooks'
 import { type ApiOutput } from '@weareinreach/api'
 import { useTranslation } from 'next-i18next'
@@ -9,6 +9,7 @@ import { z } from 'zod'
 import { Badge, GoogleMap } from '~ui/components/core'
 import { Hours } from '~ui/components/data-display'
 import { useScreenSize, useFormattedAddress } from '~ui/hooks'
+import { Icon } from '~ui/icon'
 
 const Coords = z.object({
 	lat: z.coerce.number(),
@@ -17,7 +18,7 @@ const Coords = z.object({
 
 export const VisitCard = (props: VisitCardProps) => {
 	const { isMobile } = useScreenSize()
-	const { location } = props
+	const { location, published = true } = props
 	const { t } = useTranslation(['common', 'attribute'])
 	const { ref, width } = useElementSize()
 	const formattedAddress = useFormattedAddress(location)
@@ -30,14 +31,39 @@ export const VisitCard = (props: VisitCardProps) => {
 		lng: location.longitude,
 	})
 
+	const remote = location.attributes.find(
+		({ attribute: { tsKey } }) => tsKey === 'additional.offers-remote-services'
+	)
+
+	const address = formattedAddress && (
+		<Stack spacing={12} ref={ref}>
+			<Title order={3}>{t('address', { context: remote ? 'physical' : undefined })}</Title>
+			<Text>{formattedAddress}</Text>
+			<GoogleMap marker={location} height={Math.floor(width * 0.625)} width={width} />
+		</Stack>
+	)
+
+	const remoteSection = published && remote && (
+		<Stack spacing={12}>
+			<Group spacing={6}>
+				<Icon icon={'carbon:globe'} />
+				<Title order={3}>{t(`remote`)}</Title>
+			</Group>
+			<Text>{t(remote.attribute.tsKey)}</Text>
+		</Stack>
+	)
+
 	const body = (
 		<Stack spacing={isMobile ? 32 : 40}>
 			<Title order={2}>{t('visit')}</Title>
-			<Stack spacing={12} ref={ref}>
-				<Title order={3}>{t('address')}</Title>
-				<Text>{formattedAddress}</Text>
-				<GoogleMap marker={location} height={Math.floor(width * 0.625)} width={width} />
-			</Stack>
+			{remoteSection}
+			{published ? (
+				address
+			) : (
+				<Stack spacing={12}>
+					<Text>{t('no-addresses')}</Text>
+				</Stack>
+			)}
 			<Hours data={location.hours} />
 			{/* TODO: [IN-807] Validate accessibility data points before enabling.
 			<Stack spacing={12} align='flex-start'>
@@ -64,4 +90,5 @@ type LocationResult = NonNullable<ApiOutput['location']['getById']>
 
 export type VisitCardProps = {
 	location: PageQueryResult['locations'][number] | LocationResult
+	published: boolean
 }
