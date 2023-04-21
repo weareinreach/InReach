14530ad72a770c5b3ab88fa52e0e6f198fae3fc0
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-04-21 18:28:59 -0400

    data/schema updates

---
+++ packages/db/prisma/data-migrations/2023-04-21.ts
@@ -0,0 +1,47 @@
+import { prisma, Prisma } from '~db/index'
+import { type ListrJob, type ListrTask } from '~db/prisma/dataMigrationRunner'
+import { jobPreRunner, type JobDef } from '~db/prisma/jobPreRun'
+
+/** Define the job metadata here. */
+const jobDef: JobDef = {
+	jobId: '2023-04-21-badge-render',
+	title: 'Set badge render field',
+	createdBy: 'Joe Karow',
+}
+
+const job: ListrTask = async (_ctx, task) => {
+	/** Do not edit this part - this ensures that jobs are only run once */
+	const runJob = await jobPreRunner(jobDef)
+	if (!runJob) {
+		return task.skip(`${jobDef.jobId} - Migration has already been run.`)
+	}
+	/** Start defining your data migration from here. */
+
+	const updates: Prisma.AttributeCategoryUpdateManyArgs[] = [
+		{ where: { tag: { in: ['additional-information', 'cost'] } }, data: { renderVariant: 'ATTRIBUTE' } },
+		{ where: { tag: 'organization-leadership' }, data: { renderVariant: 'LEADER' } },
+		{ where: { tag: 'service-focus' }, data: { renderVariant: 'COMMUNITY' } },
+	]
+
+	const results = await prisma.$transaction(
+		updates.map((params) => prisma.attributeCategory.updateMany(params))
+	)
+
+	const resultCount = results.reduce((prev, { count }) => prev + count, 0)
+
+	task.output = `AttributeCategory records updated: ${resultCount}`
+}
+
+/**
+ * Job export - this variable MUST be UNIQUE
+ *
+ * Use the format `jobYYYYMMDD` and append a letter afterwards if there is already a job with this name.
+ *
+ * @example `job20230404`
+ *
+ * @example `job20230404b`
+ */
+export const job20230421 = {
+	title: `${jobDef.jobId} - ${jobDef.title}`,
+	task: job,
+} satisfies ListrJob
--- packages/db/prisma/dataMigrationRunner.ts
+++ packages/db/prisma/dataMigrationRunner.ts
@@ -18,6 +18,7 @@ import {
 	job20230410b,
 	job20230411,
 	job20230411b,
+	job20230421,
 } from './data-migrations'
 
 /**
@@ -37,6 +38,7 @@ const jobList = [
 	job20230410b,
 	job20230411,
 	job20230411b,
+	job20230421,
 ]
 
 /**
