ede29bb45287011a778d366c8a722a58cf344e3b
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-04-24 17:28:50 -0400

    IN-838 display selected item

--- packages/ui/modals/dataPortal/Attributes.tsx
+++ packages/ui/modals/dataPortal/Attributes.tsx
@@ -1,13 +1,55 @@
-import { createPolymorphicComponent, type ButtonProps, Modal, Box, Stack, Select, Group } from '@mantine/core'
+import {
+	createPolymorphicComponent,
+	type ButtonProps,
+	Modal,
+	Box,
+	Stack,
+	Select,
+	Group,
+	Text,
+} from '@mantine/core'
 import { useForm } from '@mantine/form'
 import { useDisclosure } from '@mantine/hooks'
+import { type ApiOutput } from '@weareinreach/api'
 import { useTranslation } from 'next-i18next'
-import { forwardRef, useState } from 'react'
+import { forwardRef, useState, useRef, type ComponentPropsWithoutRef } from 'react'
 
+import { Badge } from '~ui/components/core/Badge'
 import { useCustomVariant } from '~ui/hooks'
+import { Icon, isValidIcon } from '~ui/icon'
 import { trpc as api } from '~ui/lib/trpcClient'
 
 import { ModalTitle } from '../ModalTitle'
+
+const SelectionItem = forwardRef<HTMLDivElement, SelectionItemProps>(({ icon, label, ...others }, ref) => {
+	const { t } = useTranslation(['attribute'])
+	return (
+		<div ref={ref} {...others}>
+			<Group
+				sx={(theme) => ({
+					alignItems: 'center',
+					gap: theme.spacing.xs,
+					padding: theme.spacing.xs,
+					borderRadius: theme.radius.sm,
+					// backgroundColor: theme.colors.gray[0],
+					cursor: 'pointer',
+					'&:hover': {
+						backgroundColor: theme.other.colors.primary.lightGray,
+					},
+				})}
+			>
+				{icon && isValidIcon(icon) && <Icon icon={icon} width={18} />}
+				{icon && !isValidIcon(icon) && <Text>{icon}</Text>}
+				{label && label}
+			</Group>
+		</div>
+	)
+})
+SelectionItem.displayName = 'SelectionItem'
+interface SelectionItemProps extends ComponentPropsWithoutRef<'div'> {
+	icon?: string
+	label: string
+}
 
 const AttributeModalBody = forwardRef<HTMLButtonElement, AttributeModalProps>(
 	({ restrictCategories, ...props }, ref) => {
@@ -15,10 +57,13 @@ const AttributeModalBody = forwardRef<HTMLButtonElement, AttributeModalProps>(
 		const [opened, handler] = useDisclosure(false)
 		const [attrCat, setAttrCat] = useState<string | undefined>()
 		const form = useForm<FormData>({ initialValues: { attributes: [], categories: [], selected: [] } })
+		const utils = api.useContext()
+		const selectAttrRef = useRef<HTMLInputElement>(null)
 
 		const { data: categories, isSuccess: categorySuccess } = api.fieldOpt.attributeCategories.useQuery(
 			restrictCategories,
 			{
+				refetchOnWindowFocus: false,
 				onSuccess: (data) => {
 					if (data.length === 1 && data[0]?.tag) {
 						setAttrCat(data[0].tag)
@@ -35,39 +80,127 @@ const AttributeModalBody = forwardRef<HTMLButtonElement, AttributeModalProps>(
 			attrCat,
 			{
 				enabled: Boolean(attrCat),
-				onSuccess: (data) =>
+				refetchOnWindowFocus: false,
+				onSuccess: (data) => {
+					const selected = form.values.selected?.map(({ value }) => value) ?? []
+					const items = data.map(
+						({
+							attributeId,
+							attributeKey,
+							icon,
+							iconBg,
+							badgeRender,
+							requireBoolean,
+							requireCountry,
+							requireData,
+							requireLanguage,
+							requireText,
+						}) => ({
+							value: attributeId,
+							label: t(attributeKey, { ns: 'attribute' }),
+							icon: icon ?? undefined,
+							iconBg: iconBg ?? undefined,
+							variant: badgeRender ?? undefined,
+							requireBoolean,
+							requireCountry,
+							requireData,
+							requireLanguage,
+							requireText,
+						})
+					)
 					form.setFieldValue(
 						'attributes',
-						data.map(({ attributeId, attributeKey, icon, badgeRender }) => ({
-							value: attributeId,
-							label: attributeKey,
-							icon: icon ?? undefined,
-							variant: badgeRender ?? undefined,
-						}))
-					),
+						items.filter(({ value }) => !selected.includes(value))
+					)
+				},
 			}
 		)
-
 		const modalTitle = <ModalTitle breadcrumb={{ option: 'close', onClick: () => handler.close() }} />
+
+		const selectHandler = (e: string | null) => {
+			if (e === null) return
+
+			const item = form.values.attributes?.find(({ value }) => value === e)
+			if (item) {
+				const { requireBoolean, requireCountry, requireData, requireLanguage, requireText } = item
+				if (requireBoolean || requireCountry || requireData || requireLanguage || requireText) return
+
+				form.setFieldValue('selected', [...form.values.selected, item])
+				form.setFieldValue(
+					'attributes',
+					form.values.attributes?.filter(({ value }) => value !== e)
+				)
+				selectAttrRef.current && (selectAttrRef.current.value = '')
+			}
+		}
+		const removeHandler = (e: string) => {
+			form.setFieldValue(
+				'selected',
+				form.values.selected?.filter(({ value }) => value !== e)
+			)
+			utils.fieldOpt.attributesByCategory.invalidate()
+		}
+
+		const selectedItems = form.values.selected?.map(({ label, icon, variant, value, iconBg }) => {
+			switch (variant) {
+				case 'ATTRIBUTE': {
+					return (
+						<Group key={value} spacing={4}>
+							<Badge variant='attribute' tsNs='attribute' tsKey={label} icon={icon ?? ''} />
+							<Icon icon='carbon:close-filled' onClick={() => removeHandler(value)} />
+						</Group>
+					)
+				}
+				case 'COMMUNITY': {
+					return (
+						<Group key={value} spacing={4}>
+							<Badge variant='community' tsKey={label} icon={icon ?? ''} />
+							<Icon icon='carbon:close-filled' onClick={() => removeHandler(value)} />
+						</Group>
+					)
+				}
+				case 'LEADER': {
+					return (
+						<Group key={value} spacing={4}>
+							<Badge variant='leader' tsKey={label} icon={icon ?? ''} iconBg={iconBg ?? ''} />
+							<Icon icon='carbon:close-filled' onClick={() => removeHandler(value)} />
+						</Group>
+					)
+				}
+				case 'LIST': {
+					return (
+						<Group key={value} spacing={4}>
+							<Text>{t(label)}</Text>
+							<Icon icon='carbon:close-filled' onClick={() => removeHandler(value)} />
+						</Group>
+					)
+				}
+			}
+		})
 
 		return (
 			<>
 				<Modal title={modalTitle} opened={opened} onClose={() => handler.close()}>
 					<Stack>
-						<Group></Group>
+						<Group>{selectedItems}</Group>
 						<Stack>
 							{form.values.categories?.length && (
 								<Select
 									data={form.values.categories}
 									onChange={(e) => (e ? setAttrCat(e) : undefined)}
 									withinPortal
+									searchable
 								/>
 							)}
 							<Select
 								data={form.values.attributes}
 								disabled={!Boolean(form.values.attributes?.length)}
 								withinPortal
-								{...form.getInputProps('selected')}
+								itemComponent={SelectionItem}
+								searchable={form.values.attributes?.length > 10}
+								ref={selectAttrRef}
+								// {...form.getInputProps('selected')}
+								onChange={selectHandler}
 							/>
 						</Stack>
 					</Stack>
@@ -90,7 +223,14 @@ interface FormData {
 		value: string
 		label: string
 		icon?: string
-		variant?: string
+		iconBg?: string
+		variant?: ApiOutput['fieldOpt']['attributesByCategory'][0]['badgeRender']
+		country?: string
+		govDist?: string
+		language?: string
+		text?: string
+		boolean?: boolean
+		data?: object
 	}[]
 	categories: {
 		value: string
@@ -100,6 +240,12 @@ interface FormData {
 		value: string
 		label: string
 		icon?: string
-		variant?: string
+		iconBg?: string
+		variant?: ApiOutput['fieldOpt']['attributesByCategory'][0]['badgeRender']
+		requireText?: boolean
+		requireLanguage?: boolean
+		requireCountry?: boolean
+		requireBoolean?: boolean
+		requireData?: boolean
 	}[]
 }
