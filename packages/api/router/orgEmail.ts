44921a71308db6d3938b00c060994e4a0f2a1a56
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-05-15 20:45:21 -0400

    add orgEmail routes

--- packages/api/router/orgEmail.ts
+++ packages/api/router/orgEmail.ts
@@ -1,3 +1,5 @@
+import { z } from 'zod'
+
 import { defineRouter, permissionedProcedure } from '~api/lib'
 import { CreateAuditLog } from '~api/schemas/create/auditLog'
 import { CreateOrgEmailSchema, UpdateOrgEmailSchema } from '~api/schemas/create/orgEmail'
@@ -38,5 +40,72 @@ export const orgEmailRouter = defineRouter({
 				return updated
 			})
 			return updatedRecord
+		}),
+	get: permissionedProcedure('createNewEmail')
+		.input(
+			z
+				.object({
+					id: z.string(),
+					organizationId: z.string(),
+					orgLocationId: z.string(),
+					serviceId: z.string(),
+				})
+				.partial()
+		)
+		.query(async ({ ctx, input }) => {
+			const { id, orgLocationId, organizationId, serviceId } = input
+
+			const result = await ctx.prisma.orgEmail.findMany({
+				where: {
+					id,
+					...(organizationId ? { organization: { some: { organizationId } } } : {}),
+					...(orgLocationId ? { locations: { some: { orgLocationId } } } : {}),
+					...(serviceId ? { services: { some: { serviceId } } } : {}),
+				},
+				select: {
+					id: true,
+					email: true,
+					firstName: true,
+					lastName: true,
+					titleId: true,
+					primary: true,
+					published: true,
+					deleted: true,
+					description: { select: { tsKey: { select: { text: true } } } },
+					locations: { select: { location: { select: { id: true, name: true } } } },
+					organization: { select: { organization: { select: { id: true, name: true, slug: true } } } },
+					services: {
+						select: {
+							service: {
+								select: {
+									id: true,
+									serviceName: { select: { tsKey: { select: { text: true } } } },
+								},
+							},
+						},
+					},
+				},
+				orderBy: [{ published: 'desc' }, { deleted: 'desc' }],
+			})
+
+			const transformedResult = result.map(
+				({ description, locations, organization, titleId, services, ...record }) => ({
+					...record,
+					description: description?.tsKey.text,
+					locations: locations.map(({ location }) => ({ ...location })),
+					organization: organization.map(({ organization }) => ({ ...organization })),
+					title: titleId,
+					services: services.map(({ service }) => ({
+						id: service.id,
+						serviceName: service.serviceName?.tsKey.text,
+					})),
+				})
+			)
+			return transformedResult
+		}),
+	upsertMany: permissionedProcedure('updateEmail')
+		.input(z.object({ orgSlug: z.string(), data: z.record(z.any()).array() }))
+		.mutation(async ({ ctx, input }) => {
+			return []
 		}),
 })
