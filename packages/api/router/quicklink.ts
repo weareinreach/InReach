a103956a19bc7660ec7c33d3ec6021f224f79e2c
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-05-18 10:35:25 -0400

    add email linking

--- packages/api/router/quicklink.ts
+++ packages/api/router/quicklink.ts
@@ -2,7 +2,7 @@ import flush from 'just-flush'
 import { z } from 'zod'
 
 import { type Prisma } from '@weareinreach/db'
-import { defineRouter, permissionedProcedure, publicProcedure } from '~api/lib/trpc'
+import { defineRouter, permissionedProcedure } from '~api/lib/trpc'
 import { CreateAuditLog } from '~api/schemas/create/auditLog'
 
 const phoneSelect = {
@@ -136,6 +136,135 @@ export const quickLinkRouter = defineRouter({
 				})
 			})
 			const results = await ctx.prisma.$transaction(updates)
+			return results
+		}),
+	getEmailData: permissionedProcedure('updateLocation')
+		.input(z.object({ limit: z.number(), skip: z.number().optional() }))
+		.query(async ({ ctx, input }) => {
+			const limit = input.limit ?? 20
+			const { skip } = input
+
+			const where = {
+				published: true,
+				deleted: false,
+				emails: { some: { email: { AND: [{ locations: { none: {} } }, { services: { none: {} } }] } } },
+			} satisfies Prisma.OrganizationWhereInput
+
+			const data = await ctx.prisma.organization.findMany({
+				where,
+				select: {
+					id: true,
+					name: true,
+					locations: { select: { id: true, name: true } },
+					services: {
+						select: {
+							id: true,
+							serviceName: { select: { tsKey: { select: { text: true } } } },
+						},
+					},
+					emails: {
+						select: {
+							email: {
+								select: {
+									id: true,
+									email: true,
+									firstName: true,
+									lastName: true,
+									locationOnly: true,
+									serviceOnly: true,
+									description: { select: { tsKey: { select: { text: true } } } },
+									locations: { select: { location: { select: { id: true } } } },
+									services: { select: { service: { select: { id: true } } } },
+								},
+							},
+						},
+					},
+				},
+				orderBy: { id: 'asc' },
+				take: limit, // + 1,
+				skip,
+			})
+			const totalResults = await ctx.prisma.organization.count({ where })
+			const transformedData = data.flatMap(({ locations, emails, services, id, name }) =>
+				emails.map(({ email }) => {
+					const {
+						description,
+						id: emailId,
+						locations: attachedLocations,
+						services: attachedServices,
+						...rest
+					} = email
+					return {
+						orgId: id,
+						name,
+						emailId,
+						...rest,
+						description: description?.tsKey.text,
+						attachedLocations: attachedLocations.map(({ location }) => location.id),
+						attachedServices: attachedServices.map(({ service }) => service.id),
+						locations,
+						services,
+					}
+				})
+			)
+			return {
+				results: transformedData,
+				totalResults,
+			}
+		}),
+	updateEmailData: permissionedProcedure('updateLocation')
+		.input(
+			z
+				.object({
+					id: z.string(),
+					from: z
+						.object({
+							serviceOnly: z.boolean().optional(),
+							locationOnly: z.boolean().optional(),
+							locations: z.string().array(),
+							services: z.string().array(),
+						})
+						.partial(),
+					to: z.object({
+						serviceOnly: z.boolean().optional(),
+						locationOnly: z.boolean().optional(),
+						locations: z.object({ add: z.string().array(), del: z.string().array() }).partial(),
+						services: z.object({ add: z.string().array(), del: z.string().array() }).partial(),
+					}),
+				})
+				.array()
+		)
+		.mutation(async ({ ctx, input }) => {
+			const updates = input.map(({ id, from, to }) => {
+				const { serviceOnly, locationOnly, locations, services } = to
+				const auditLogs = CreateAuditLog({ actorId: ctx.actorId, operation: 'UPDATE', from, to: flush(to) })
+				return ctx.prisma.orgEmail.update({
+					where: { id },
+					data: {
+						serviceOnly,
+						locationOnly,
+						locations: {
+							createMany: locations.add?.length
+								? { data: locations.add.map((orgLocationId) => ({ orgLocationId })), skipDuplicates: true }
+								: undefined,
+							deleteMany: locations.del?.length ? { orgLocationId: { in: locations.del } } : undefined,
+						},
+						services: {
+							createMany: services.add?.length
+								? { data: services.add.map((serviceId) => ({ serviceId })), skipDuplicates: true }
+								: undefined,
+							deleteMany: services.del?.length ? { serviceId: { in: services.del } } : undefined,
+						},
+						auditLogs,
+					},
+				})
+			})
+			const results = await ctx.prisma.$transaction(updates)
+
+			ctx.prisma.orgLocationService.groupBy({
+				by: ['serviceId'],
+				having: { orgLocationId: { _count: { gte: 2 } } },
+			})
 			return results
 		}),
 })
