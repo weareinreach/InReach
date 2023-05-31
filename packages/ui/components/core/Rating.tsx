2298e62202708abb984661b21725f26061622150
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-05-31 11:29:33 -0400

    IN-896 update "No reviews" state

--- packages/ui/components/core/Rating.tsx
+++ packages/ui/components/core/Rating.tsx
@@ -17,32 +17,52 @@ const useStyles = createStyles((theme, { noMargin }: Partial<Props>) => ({
 	text: {
 		...theme.other.utilityFonts.utility1,
 	},
+	iconDimmed: {
+		color: theme.other.colors.tertiary.coolGray,
+	},
+	textDimmed: {
+		color: theme.other.colors.secondary.darkGray,
+	},
 }))
 
 export const Rating = ({ recordId, hideCount = false, noMargin = false, forceLoading = false }: Props) => {
-	const { classes } = useStyles({ noMargin })
+	const { classes, cx } = useStyles({ noMargin })
 	const { t } = useTranslation('common')
 	const variants = useCustomVariant()
 	const { data, status } = api.review.getAverage.useQuery(recordId as string, { enabled: Boolean(recordId) })
 
-	const { average, count } = data ?? { average: 0, count: 0 }
+	const { average, count } = data ?? { count: 0 }
 
 	const parenRegex = /\(|\)/g
+
+	if (status !== 'success' || Boolean(forceLoading)) {
+		return <Skeleton className={classes.container} visible={true} />
+	}
+
+	if (average === null || count === 0) {
+		return (
+			<Group position='center' spacing={5} className={classes.container}>
+				<Icon icon='carbon:star-filled' className={cx(classes.icon, classes.iconDimmed)} height={24} />
+				<Text className={cx(classes.text, classes.textDimmed)}>
+					{t('review-count_interval', { count, postProcess: 'interval' })}
+				</Text>
+			</Group>
+		)
+	}
+
 	return (
 		<Tooltip
 			label={t('review-count_interval', { count, postProcess: 'interval' }).replace(parenRegex, '')}
 			disabled={!hideCount}
 			variant={variants.Tooltip.utility1}
 		>
-			<Skeleton visible={status !== 'success' || Boolean(forceLoading)} className={classes.container}>
-				<Group position='center' spacing={5} className={classes.container}>
-					<Icon icon='carbon:star-filled' className={classes.icon} height={24} />
-					<Text className={classes.text}>
-						{average === null && hideCount ? '-.-' : average}{' '}
-						{!hideCount && `${t('review-count_interval', { count, postProcess: 'interval' })}`}
-					</Text>
-				</Group>
-			</Skeleton>
+			<Group position='center' spacing={5} className={classes.container}>
+				<Icon icon='carbon:star-filled' className={classes.icon} height={24} />
+				<Text className={classes.text}>
+					{average === null && hideCount ? '-.-' : average}{' '}
+					{!hideCount && `${t('review-count_interval', { count, postProcess: 'interval' })}`}
+				</Text>
+			</Group>
 		</Tooltip>
 	)
 }
