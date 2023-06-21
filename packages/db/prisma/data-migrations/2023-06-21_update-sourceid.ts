5132d40b2fee2ba99eff9d22df5c1cce9ac40b77
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-06-21 16:04:25 -0400

    data migration: update sourceId

---
+++ packages/db/prisma/data-migrations/2023-06-21_update-sourceid.ts
@@ -0,0 +1,55 @@
+import { prisma } from '~db/index'
+import { isSuccess } from '~db/prisma/common'
+import { type ListrJob, type ListrTask } from '~db/prisma/dataMigrationRunner'
+import { type JobDef, jobPostRunner, jobPreRunner } from '~db/prisma/jobPreRun'
+
+/** Define the job metadata here. */
+const jobDef: JobDef = {
+	jobId: '2023-06-21_update-sourceid',
+	title: 'Update source id for migration',
+	createdBy: 'Joe Karow',
+}
+/**
+ * Job export - this variable MUST be UNIQUE
+ *
+ * Use the format `jobYYYYMMDD` and append a letter afterwards if there is already a job with this name.
+ *
+ * @example `job20230404`
+ *
+ * @example `job20230404b`
+ */
+export const job20230621a = {
+	title: `${jobDef.jobId} - ${jobDef.title}`,
+	task: async (_ctx, task) => {
+		/**
+		 * Do not edit this part
+		 *
+		 * This ensures that jobs are only run once
+		 */
+		if (await jobPreRunner(jobDef, task)) {
+			return task.skip(`${jobDef.jobId} - Migration has already been run.`)
+		}
+		/**
+		 * Start defining your data migration from here.
+		 *
+		 * To log output, use `task.output = 'Message to log'`
+		 *
+		 * This will be written to `stdout` and to a log file in `/prisma/migration-logs/`
+		 */
+
+		// Do stuff
+
+		const update = await prisma.source.update({
+			where: { id: 'clfii2q0e00009kjupxn7403t' },
+			data: { id: 'srce_01H3FR96DFKRAM07AZ8T8MXHE5' },
+		})
+		task.output = `${isSuccess(update)} Source record updated`
+
+		/**
+		 * DO NOT REMOVE BELOW
+		 *
+		 * This writes a record to the DB to register that this migration has run successfully.
+		 */
+		await jobPostRunner(jobDef)
+	},
+} satisfies ListrJob
