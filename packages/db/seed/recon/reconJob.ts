019e2f1efdb14bc61e697024341d4d69e81cbcbd
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-06-27 17:41:13 -0400

    reconcile org basic fields

--- packages/db/seed/recon/reconJob.ts
+++ packages/db/seed/recon/reconJob.ts
@@ -5,13 +5,15 @@ import fs from 'fs'
 import path from 'path'
 
 import { prisma, type Prisma } from '~db/client'
+import { namespace } from '~db/generated/namespaces'
 import { createPoint } from '~db/lib/createPoint'
-import { existingOrgsSchema } from '~db/seed/recon/input/types'
+import { organizationsSchema } from '~db/seed/recon/input/types'
 import { needsUpdate } from '~db/seed/recon/lib/compare'
 import { dataCorrections, existing, getCountryId, getGovDistId } from '~db/seed/recon/lib/existing'
 import { attachLogger, formatMessage } from '~db/seed/recon/lib/logger'
 import { create, update, writeBatches } from '~db/seed/recon/lib/output'
 import { type ListrJob } from '~db/seed/recon/lib/types'
+import { trimSpaces } from '~db/seed/recon/lib/utils'
 import { JsonInputOrNull } from '~db/zod_util/prismaJson'
 
 export const orgRecon = {
@@ -21,12 +23,12 @@ export const orgRecon = {
 		attachLogger(task)
 		const log = (...args: Parameters<typeof formatMessage>) => (task.output = formatMessage(...args))
 		const logUpdate = (field: string, from: unknown, to: unknown) =>
-			log(`Updating ${field} from "${from}" to "${to}"`, undefined, true)
+			log(`Updating ${field} from "${from}" to "${to}"`, 'update', true)
 		writeBatches(task, true)
 		let orgCounter = 1
 
 		// read input file
-		const orgs = existingOrgsSchema
+		const orgs = organizationsSchema
 			.array()
 			.parse(JSON.parse(fs.readFileSync(path.resolve(__dirname, './input/existingOrgs.json'), 'utf-8')))
 		log(`Organizations to process: ${orgs.length}`, 'info')
@@ -34,13 +36,75 @@ export const orgRecon = {
 		// start loop
 		for (const org of orgs) {
 			task.title = `Reconcile Organizations [${orgCounter}/${orgs.length}]`
-			log(`Reconciling Organization: ${org.name}`)
+			log(`Reconciling Organization: ${org.name}`, 'generate')
 
 			const organizationId = existing.orgId.get(org._id.$oid)
 			if (!organizationId) {
 				log(`Organization not found: ${org._id.$oid}`, 'error')
 				throw new Error('Organization not found', { cause: org._id.$oid })
 			}
+			/**
+			 * .
+			 *
+			 * === Update basic org fields ===
+			 *
+			 * .
+			 */
+			// #region Organization basics
+			const existingOrgRecord = await prisma.organization.findUniqueOrThrow({
+				where: { id: organizationId },
+				include: { description: { include: { tsKey: true } } },
+			})
+			const updateOrgRecord: Prisma.OrganizationUpdateArgs = {
+				where: { id: organizationId },
+				data: {},
+			}
+
+			if (needsUpdate(existingOrgRecord.name, trimSpaces(org.name))) {
+				logUpdate('name', existingOrgRecord.name, trimSpaces(org.name))
+				updateOrgRecord.data.name = trimSpaces(org.name)
+			}
+			if (
+				existingOrgRecord.description &&
+				needsUpdate(existingOrgRecord.description.tsKey.text, trimSpaces(org.description))
+			) {
+				logUpdate('description', existingOrgRecord.description.tsKey.text, trimSpaces(org.description))
+				update.translationKey.add({
+					where: { ns_key: { ns: namespace.orgData, key: existingOrgRecord.description.key } },
+					data: { text: trimSpaces(org.description) },
+				})
+			}
+			if (needsUpdate(existingOrgRecord.deleted, org.is_deleted)) {
+				logUpdate('deleted', existingOrgRecord.deleted, org.is_deleted)
+				updateOrgRecord.data.deleted = org.is_deleted
+			}
+			if (needsUpdate(existingOrgRecord.published, org.is_published)) {
+				if (org.is_published === true) {
+					log(`SKIPPING - Mark organization as published`, 'skip', true)
+				} else {
+					logUpdate('published', existingOrgRecord.published, org.is_published)
+					updateOrgRecord.data.published = org.is_published
+				}
+			}
+			if (org.verified_at?.$date && needsUpdate(existingOrgRecord.lastVerified, org.verified_at?.$date)) {
+				logUpdate('lastVerified', existingOrgRecord.lastVerified, org.verified_at.$date)
+				updateOrgRecord.data.lastVerified = org.verified_at.$date
+			}
+
+			if (Object.keys(updateOrgRecord.data).length) {
+				update.organization.add(updateOrgRecord)
+				log(`Updated ${Object.keys(updateOrgRecord.data).length} keys`, undefined, true)
+			}
+
+			// #endregion
+
+			/**
+			 * .
+			 *
+			 * === LOCATION ===
+			 *
+			 * .
+			 */
 			// #region Location
 			if (!org.locations.length) {
 				log('SKIPPING Location records, no locations', 'skip')
@@ -90,25 +154,25 @@ export const orgRecon = {
 							(x) => +parseFloat(x.$numberDecimal).toFixed(3)
 						)
 
-						if (needsUpdate(existingLocation.name, location.name)) {
-							logUpdate('name', existingLocation.name, location.name)
-							updateRecord.data.name = location.name?.trim() ?? null
+						if (needsUpdate(existingLocation.name, trimSpaces(location.name))) {
+							logUpdate('name', existingLocation.name, trimSpaces(location.name))
+							updateRecord.data.name = trimSpaces(location.name) ?? null
 						}
 						if (needsUpdate(existingLocation.street1, location.address)) {
 							logUpdate('street1', existingLocation.street1, location.address)
-							updateRecord.data.street1 = location.address?.trim()
+							updateRecord.data.street1 = trimSpaces(location.address)
 						}
 						if (needsUpdate(existingLocation.street2, location.unit)) {
 							logUpdate('street2', existingLocation.street2, location.unit)
-							updateRecord.data.street2 = location.unit?.trim() ?? null
+							updateRecord.data.street2 = trimSpaces(location.unit) ?? null
 						}
 						if (needsUpdate(existingLocation.city, location.city)) {
 							logUpdate('city', existingLocation.city, location.city)
-							updateRecord.data.city = location.city?.trim()
+							updateRecord.data.city = trimSpaces(location.city)
 						}
 						if (needsUpdate(existingLocation.postCode, location.zip_code)) {
 							logUpdate('postCode', existingLocation.postCode, location.zip_code)
-							updateRecord.data.postCode = location.zip_code?.trim() ?? null
+							updateRecord.data.postCode = trimSpaces(location.zip_code) ?? null
 						}
 						if (needsUpdate(existingLocation.primary, location.is_primary)) {
 							logUpdate('primary', existingLocation.primary, location.is_primary)
