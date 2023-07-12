1a86878aadb2712e793b46855b8a993f4ef2ead0
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-07-12 13:41:33 -0400

    add raise util

--- packages/db/prisma/common.ts
+++ packages/db/prisma/common.ts
@@ -1,1 +1,10 @@
 export const isSuccess = (criteria: unknown) => (criteria ? '✅' : '❎')
+
+/**
+ * Throws an error - Use to raise an error for nullish-coalesced values
+ *
+ * @example FnThatDoesntAcceptUndefined(possibleUndefined ?? raise('this is the error'))
+ */
+export const raise = (err: string): never => {
+	throw new Error(err)
+}
