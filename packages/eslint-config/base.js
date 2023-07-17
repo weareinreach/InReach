2f22911a020687a99145d931055a3b019ceb49f7
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-07-17 11:27:14 -0400

    fix eslint plugin error

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unused-modules */
 /** @type {import('eslint').ESLint.ConfigData} */
 const config = {
-	plugins: ['codegen', 'turbo', 'node', 'import', '@tanstack/query'],
+	plugins: ['codegen', 'turbo', 'node', /*'import',*/ '@tanstack/query'],
 	extends: [
 		'eslint:recommended',
 		// disable turbo plugin until vercel/turbo#5355 is resolved
