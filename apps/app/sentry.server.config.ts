4212d81296a72f5d3a7ed8c931241866bfb0318b
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-07-28 17:22:07 -0400

    update config

--- apps/app/sentry.server.config.ts
+++ apps/app/sentry.server.config.ts
@@ -12,6 +12,4 @@ Sentry.init({
 
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
 	debug: false,
-	// eslint-disable-next-line node/no-process-env
-	enabled: process.env.NODE_ENV === 'production',
 })
