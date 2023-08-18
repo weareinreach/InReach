86eaa3ecfd2a8bafc5fc74b1809e3292aa3523f4
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-08-18 10:47:49 -0400

    extract login body, create pages for 401, 403 errors

--- apps/app/src/types/nextjs-routes.d.ts
+++ apps/app/src/types/nextjs-routes.d.ts
@@ -11,6 +11,8 @@ declare module "nextjs-routes" {
   } from "next";
 
   export type Route =
+    | StaticRoute<"/401">
+    | StaticRoute<"/403">
     | StaticRoute<"/404">
     | StaticRoute<"/500">
     | StaticRoute<"/account">
