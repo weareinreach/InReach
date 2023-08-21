87d5f141c1c8dc1b9c2149d3f41d2158c9b704fe
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-08-21 18:07:50 -0400

    log display during dev mode

--- packages/util/logger/index.ts
+++ packages/util/logger/index.ts
@@ -1,6 +1,13 @@
 import { type ISettingsParam, Logger } from 'tslog'
 
-export const appLog = new Logger({ name: 'app', type: 'json', hideLogPositionForProduction: true })
+// eslint-disable-next-line node/no-process-env
+const isDev = process.env.NODE_ENV === 'development'
+
+export const appLog = new Logger({
+	name: 'app',
+	type: isDev ? 'pretty' : 'json',
+	hideLogPositionForProduction: true,
+})
 
 export const createLoggerInstance = (name: string, opts?: Omit<ISettingsParam<unknown>, 'name'>) =>
 	appLog.getSubLogger({ name, ...opts })
