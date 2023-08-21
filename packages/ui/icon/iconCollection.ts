0787e23830d47fbdf8a280d7953313fee8f53563
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-08-21 17:57:56 -0400

    add deps & icons

--- packages/ui/icon/iconCollection.ts
+++ packages/ui/icon/iconCollection.ts
@@ -3,6 +3,7 @@ import { addCollection } from '@iconify/react'
 import { getIcons } from '@iconify/utils'
 
 const { icons: carbonIcons } = require('@iconify-json/carbon')
+const { icons: fluentIcons } = require('@iconify-json/fluent-mdl2')
 const { icons: mdiIcons } = require('@iconify-json/mdi')
 const { icons: phIcons } = require('@iconify-json/ph')
 const { icons: simpleIcons } = require('@iconify-json/simple-icons')
@@ -15,10 +16,12 @@ const { icons: simpleIcons } = require('@iconify-json/simple-icons')
 export const loadIcons = () => {
 	const iconsSimple = getIcons(simpleIcons, ['tiktok'])
 	const iconsPh = getIcons(phIcons, ['map-pin-fill'])
-	const iconsMdi = getIcons(mdiIcons, ['map-marker'])
+	const iconsMdi = getIcons(mdiIcons, ['map-marker', 'content-save'])
+	const iconsFluent = getIcons(fluentIcons, ['remove-from-trash'])
 
 	addCollection(carbonIcons)
 	if (iconsSimple !== null) addCollection(iconsSimple)
 	if (iconsPh !== null) addCollection(iconsPh)
 	if (iconsMdi !== null) addCollection(iconsMdi)
+	if (iconsFluent !== null) addCollection(iconsFluent)
 }
