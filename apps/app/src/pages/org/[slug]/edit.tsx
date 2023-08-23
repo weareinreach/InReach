3b0d88eff6bd942a1549b4ec4bf544aff2f31031
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-08-23 18:21:00 -0400

    basic info inline edits

--- apps/app/src/pages/org/[slug]/edit.tsx
+++ apps/app/src/pages/org/[slug]/edit.tsx
@@ -1,34 +1,46 @@
-/* eslint-disable i18next/no-literal-string */
-import { Grid, Stack, Tabs, Title } from '@mantine/core'
+import { DevTool } from '@hookform/devtools'
+import { Grid, Stack } from '@mantine/core'
 import { useElementSize } from '@mantine/hooks'
+import compact from 'just-compact'
 import { type GetServerSideProps } from 'next'
 import { useRouter } from 'next/router'
 import { useTranslation } from 'next-i18next'
 import { type RoutedQuery } from 'nextjs-routes'
 import { useEffect, useState } from 'react'
+import { FormProvider, useForm } from 'react-hook-form'
+import { z } from 'zod'
 
 import { trpcServerClient } from '@weareinreach/api/trpc'
 import { checkServerPermissions } from '@weareinreach/auth'
 import { GoogleMap } from '@weareinreach/ui/components/core/GoogleMap'
-import { Toolbar } from '@weareinreach/ui/components/core/Toolbar'
 import { ContactSection } from '@weareinreach/ui/components/sections/Contact'
 import { ListingBasicInfo } from '@weareinreach/ui/components/sections/ListingBasicInfo'
 import { LocationCard } from '@weareinreach/ui/components/sections/LocationCard'
-import { PhotosSection } from '@weareinreach/ui/components/sections/Photos'
-import { ReviewSection } from '@weareinreach/ui/components/sections/Reviews'
-import { ServicesInfoCard } from '@weareinreach/ui/components/sections/ServicesInfo'
-import { VisitCard } from '@weareinreach/ui/components/sections/VisitCard'
+import { OrgPageLoading } from '@weareinreach/ui/loading-states/OrgPage'
 import { type NextPageWithOptions } from '~app/pages/_app'
 import { api } from '~app/utils/api'
 import { getServerSideTranslations } from '~app/utils/i18n'
 
+const formSchema = z
+	.object({
+		name: z.string(),
+		description: z.string(),
+	})
+	.partial()
+type FormSchema = z.infer<typeof formSchema>
+
 const OrganizationPage: NextPageWithOptions = () => {
-	const { t } = useTranslation()
 	const router = useRouter<'/org/[slug]'>()
 	const { query } = router.isReady ? router : { query: { slug: '' } }
-	const [activeTab, setActiveTab] = useState<string | null>('services')
-	const [loading, setLoading] = useState(true)
 	const { data, status } = api.organization.forOrgPageEdits.useQuery(query, { enabled: router.isReady })
+	const { t } = useTranslation()
+	const formMethods = useForm<FormSchema>({
+		values: {
+			name: data?.name,
+			description: data?.description?.tsKey?.text,
+		},
+	})
+	const [loading, setLoading] = useState(true)
 	const { data: hasRemote } = api.service.forServiceInfoCard.useQuery(
 		{ parentId: data?.id ?? '', remoteOnly: true },
 		{
@@ -40,41 +52,15 @@ const OrganizationPage: NextPageWithOptions = () => {
 	useEffect(() => {
 		if (data && status === 'success') setLoading(false)
 	}, [data, status])
-	if (loading || !data) return <>Loading</>
+	if (loading || !data) return <OrgPageLoading />
 
-	const { attributes, description, slug, reviews, locations, isClaimed, id: organizationId } = data
-
-	const sidebar =
-		locations?.length === 1 ? (
-			<>{locations[0] && <VisitCard locationId={locations[0].id} />}</>
-		) : (
-			locations.length && (
-				<Stack ref={ref} miw='100%'>
-					{width && (
-						<GoogleMap
-							locationIds={locations.map(({ id }) => id)}
-							width={width}
-							height={Math.floor(width * 1.185)}
-						/>
-					)}
-				</Stack>
-			)
-		)
+	const { attributes, description, slug, locations, isClaimed } = data
 
 	return (
-		<>
-			<Grid.Col sm={12} order={0}>
-				<Title order={1}>EDIT MODE</Title>
-			</Grid.Col>
+		<FormProvider {...formMethods}>
 			<Grid.Col sm={8} order={1}>
-				{/* <Toolbar
-					breadcrumbProps={{ option: 'back', backTo: 'search', onClick: () => router.back() }}
-					saved={Boolean(userLists?.length)}
-					organizationId={organizationId}
-				/> */}
 				<Stack pt={24} align='flex-start' spacing={40}>
 					<ListingBasicInfo
-						role='org'
 						data={{
 							name: data.name,
 							id: data.id,
@@ -85,21 +71,32 @@ const OrganizationPage: NextPageWithOptions = () => {
 							locations,
 							isClaimed,
 						}}
+						edit
 					/>
-					{locations.map((location) => (
-						<LocationCard key={location.id} locationId={location.id} />
-					))}
+					<Stack spacing={40}>
+						{locations.map((location) => (
+							<LocationCard key={location.id} locationId={location.id} />
+						))}
+						{hasRemote && <LocationCard remoteOnly />}
+					</Stack>
 				</Stack>
 			</Grid.Col>
 			<Grid.Col order={2}>
 				<Stack spacing={40}>
 					<ContactSection role='org' parentId={data.id} />
-					{/* </Grid.Col> */}
-					{/* <Grid.Col order={4}> */}
-					{sidebar}
+					<Stack ref={ref} miw='100%'>
+						{width && (
+							<GoogleMap
+								locationIds={locations.map(({ id }) => id)}
+								width={width}
+								height={Math.floor(width * 1.185)}
+							/>
+						)}
+					</Stack>
 				</Stack>
+				<DevTool control={formMethods.control} />
 			</Grid.Col>
-		</>
+		</FormProvider>
 	)
 }
 
@@ -126,12 +123,17 @@ export const getServerSideProps: GetServerSideProps<
 	}
 
 	const ssg = await trpcServerClient({ session })
+	const orgId = await ssg.organization.getIdFromSlug.fetch({ slug })
 
-	await ssg.organization.getBySlug.prefetch({ slug })
+	const [i18n] = await Promise.allSettled([
+		getServerSideTranslations(locale, compact(['common', 'services', 'attribute', 'phone-type', orgId?.id])),
+		ssg.organization.getBySlug.prefetch({ slug }),
+		ssg.organization.forOrgPageEdits.prefetch({ slug }),
+	])
 	const props = {
 		session,
 		trpcState: ssg.dehydrate(),
-		...(await getServerSideTranslations(locale, ['common', 'services', 'attribute', 'phone-type', slug])),
+		...(i18n.status === 'fulfilled' ? i18n.value : {}),
 	}
 
 	return {
--- packages/ui/components/sections/ListingBasicInfo.tsx
+++ packages/ui/components/sections/ListingBasicInfo.tsx
@@ -1,17 +1,20 @@
 import { Skeleton, Stack, Text, Title } from '@mantine/core'
 import { useTranslation } from 'next-i18next'
+import { memo } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { Textarea, TextInput } from 'react-hook-form-mantine'
 
 import { type ApiOutput } from '@weareinreach/api'
 import { BadgeGroup, type CustomBadgeProps } from '~ui/components/core/Badge'
 import { Rating } from '~ui/components/core/Rating'
+import { InlineTextInput } from '~ui/components/data-portal/InlineTextInput'
 import { useCustomVariant, useFormattedAddress } from '~ui/hooks'
 
-export const ListingBasicInfo = ({ role, data }: ListingBasicInfoProps) => {
+export const ListingBasicDisplay = memo(({ data }: ListingBasicInfoProps) => {
 	const { t, ready: i18nReady } = useTranslation(data.id)
 	const variants = useCustomVariant()
-	const { attributes, isClaimed, locations, description, slug, id } = data
+	const { attributes, isClaimed, locations, description, id } = data
 
-	// const isMultiLoc = role === 'org' && (locations?.length ?? 0) > 1
 	const isSingleLoc = locations?.length === 1
 	const location = isSingleLoc ? locations[0] : undefined
 
@@ -72,34 +75,97 @@ export const ListingBasicInfo = ({ role, data }: ListingBasicInfoProps) => {
 			<BadgeGroup badges={focusedCommBadges} />
 		</Stack>
 	)
-}
+})
+ListingBasicDisplay.displayName = 'ListingBasicDisplay'
 
-export type ListingBasicInfoProps = { role: 'org' | 'location' } & (OrgInfoProps | LocationInfoProps)
+export const ListingBasicEdit = ({ data }: ListingBasicInfoProps) => {
+	const form = useFormContext()
+	const { attributes, isClaimed } = data
+
+	const leaderAttributes = attributes.filter(({ attribute }) =>
+		attribute.categories.some(({ category }) => category.tag === 'organization-leadership')
+	)
+	const focusedCommunities = attributes.filter(({ attribute }) =>
+		attribute.categories.some(
+			({ category }) => category.tag === 'service-focus' && attribute._count.parents === 0
+		)
+	)
+
+	const infoBadges = () => {
+		const output: CustomBadgeProps[] = []
+		if (leaderAttributes.length) {
+			leaderAttributes.forEach((entry) =>
+				output.push({
+					variant: 'leader',
+					icon: entry.attribute.icon ?? '',
+					iconBg: entry.attribute.iconBg ?? '#FFF',
+					tsKey: entry.attribute.tsKey,
+				})
+			)
+		}
+		if (data.lastVerified)
+			output.push({
+				variant: 'verified',
+				lastverified: data.lastVerified.toString(),
+			})
+		output.push({
+			variant: isClaimed ? 'claimed' : 'unclaimed',
+		})
+
+		return output
+	}
+	const focusedCommBadges: CustomBadgeProps[] = focusedCommunities.map(({ attribute }) => ({
+		variant: 'community',
+		tsKey: attribute.tsKey,
+		icon: attribute.icon ?? '',
+	}))
+
+	return (
+		<form autoComplete='off'>
+			<Stack align='flex-start' spacing={12}>
+				<InlineTextInput
+					component={TextInput}
+					name='name'
+					control={form.control}
+					fontSize='h2'
+					data-isDirty={form.formState.dirtyFields['name']}
+				/>
+				<BadgeGroup badges={infoBadges()} withSeparator />
+				<InlineTextInput
+					component={Textarea}
+					name='description'
+					control={form.control}
+					autosize
+					data-isDirty={form.formState.dirtyFields['description']}
+				/>
+				<BadgeGroup badges={focusedCommBadges} />
+			</Stack>
+		</form>
+	)
+}
+ListingBasicEdit.displayName = 'ListingBasicEdit'
+
+export const ListingBasicInfo = ({ data, edit }: ListingBasicInfoProps) =>
+	edit ? <ListingBasicEdit data={data} /> : <ListingBasicDisplay data={data} />
+export type ListingBasicInfoProps = { edit?: boolean } & OrgInfoProps
 
 export interface OrgInfoProps {
-	role: 'org'
 	data: {
 		name: string
 		id: string
 		slug: string
-		lastVerified: NonNullable<ApiOutput['organization']['forOrgPage']>['lastVerified']
-		attributes: NonNullable<ApiOutput['organization']['forOrgPage']>['attributes']
-		description: NonNullable<ApiOutput['organization']['forOrgPage']>['description']
-		locations: NonNullable<ApiOutput['organization']['forOrgPage']>['locations']
-		isClaimed: boolean
-	}
-}
-
-export interface LocationInfoProps {
-	role: 'location'
-	data: {
-		name: string
-		id: string
-		slug: string
-		lastVerified: NonNullable<ApiOutput['organization']['forLocationPage']>['lastVerified']
-		attributes: NonNullable<ApiOutput['location']['forLocationPage']>['attributes']
-		description: NonNullable<ApiOutput['location']['forLocationPage']>['description']
-		locations: NonNullable<ApiOutput['location']['forLocationPage']>[]
+		lastVerified:
+			| NonNullable<ApiOutput['organization']['forOrgPage']>['lastVerified']
+			| NonNullable<ApiOutput['organization']['forLocationPage']>['lastVerified']
+		attributes:
+			| NonNullable<ApiOutput['organization']['forOrgPage']>['attributes']
+			| NonNullable<ApiOutput['location']['forLocationPage']>['attributes']
+		description:
+			| NonNullable<ApiOutput['organization']['forOrgPage']>['description']
+			| NonNullable<ApiOutput['location']['forLocationPage']>['description']
+		locations:
+			| NonNullable<ApiOutput['organization']['forOrgPage']>['locations']
+			| NonNullable<ApiOutput['location']['forLocationPage']>[]
 		isClaimed: boolean
 	}
 }
