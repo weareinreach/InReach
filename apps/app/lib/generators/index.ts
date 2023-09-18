52768d01cedf3948f45f1879bb4161dcfdddaaeb
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-09-18 17:01:32 -0400

    clean up generators

--- apps/app/lib/generators/index.ts
+++ apps/app/lib/generators/index.ts
@@ -1,7 +1,3 @@
-/* eslint-disable import/no-unused-modules */
 // codegen:start {preset: barrel, include: ./*.ts}
-export * from './footer'
-export * from './navigation'
-export * from './socialMedia'
 export * from './translationKeys'
 // codegen:end
