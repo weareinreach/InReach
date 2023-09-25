5b263e437b14f28335ea7aaf68985e4bc61ede82
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-09-25 11:22:00 -0400

    clean up comments/dead code

--- packages/ui/components/data-portal/PhoneNumberEntryHookForm.tsx
+++ packages/ui/components/data-portal/PhoneNumberEntryHookForm.tsx
@@ -3,17 +3,9 @@ import { ErrorMessage } from '@hookform/error-message'
 import { createStyles, Group, rem, Text, TextInput, type TextInputProps } from '@mantine/core'
 import { AsYouType } from 'libphonenumber-js'
 import { type ComponentPropsWithoutRef, forwardRef, useEffect, useMemo } from 'react'
-import { type FieldValues, type Path, useFormContext, useFormState } from 'react-hook-form'
-import {
-	Select,
-	type SelectProps,
-	// TextInput,
-	// type TextInputProps,
-} from 'react-hook-form-mantine'
-import PhoneInput, {
-	// parsePhoneNumber,
-	type Props as PhoneInputProps,
-} from 'react-phone-number-input/react-hook-form-input'
+import { type FieldValues, type Path, useFormContext } from 'react-hook-form'
+import { Select, type SelectProps } from 'react-hook-form-mantine'
+import PhoneInput, { type Props as PhoneInputProps } from 'react-phone-number-input/react-hook-form-input'
 
 import { type ApiOutput } from '@weareinreach/api'
 import { isCountryCode } from '~ui/hooks/usePhoneNumber'
@@ -28,7 +20,6 @@ const PhoneCountrySelectItem = forwardRef<HTMLDivElement, PhoneCountrySelectItem
 			<Group ref={ref} {...props} w='100%' noWrap>
 				<Text>{label}</Text>
 				<Text>{name}</Text>
-				{/* <Text>{`${label} ${name}`}</Text> */}
 			</Group>
 		)
 	}
@@ -66,18 +57,11 @@ export const PhoneNumberEntry = <T extends FieldValues>({
 	phoneEntryProps: allPhoneEntryProps,
 }: PhoneNumberEntryProps<T>) => {
 	const form = useFormContext<T>()
-	// const selectedCountry = useWatch<T>({control: countrySelectProps.control, name: countrySelectProps.name})
 	const selectedCountry = form.watch(countrySelectProps.name)
 	const phoneNumber = form.watch(allPhoneEntryProps.name)
-	// const [selectedCountry, setSelectedCountry] = useState<CountryCode | undefined>()
 	const { classes: countrySelectClasses } = useCountrySelectStyles()
 	const { classes: phoneEntryClasses } = usePhoneEntryStyles()
-	const {
-		setError: setPhoneError,
-		// value: phoneValue,
-		// onChange: onPhoneChange,
-		...phoneEntryProps
-	} = allPhoneEntryProps
+	const { setError: setPhoneError, ...phoneEntryProps } = allPhoneEntryProps
 	const topCountries = ['US', 'CA', 'MX']
 
 	const { data: countryList } = api.fieldOpt.countries.useQuery(
@@ -108,43 +92,25 @@ export const PhoneNumberEntry = <T extends FieldValues>({
 	}, [selectedCountry, countryList])
 
 	const phoneFormatter = new AsYouType(activeCountry)
-	// useEffect(() => {
-	// 	const { data } = countryList.find(({ value }) => value === countrySelectProps.value) ?? {}
-	// 	if (data?.cca2 && isCountryCode(data.cca2)) setSelectedCountry(data.cca2)
-	// 	else if (data === undefined) setSelectedCountry(undefined)
-	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	// }, [countrySelectProps.value])
 
 	useEffect(() => {
 		if (phoneNumber) {
 			phoneFormatter.input(phoneNumber)
 			const phoneCountry = phoneFormatter.getNumber()?.country
-
-			console.log(
-				`🚀 ~ file: PhoneNumberEntryHookForm.tsx:124 ~ useEffect ~ phoneCountry/selectedCountry:`,
-				phoneCountry,
-				selectedCountry
-			)
 			const formError = form.formState.errors
-			console.log(formError)
 			if ((!phoneCountry && !selectedCountry) || phoneCountry !== selectedCountry) {
 				const countryId = countryList.find(({ data }) => data.cca2 === phoneCountry)?.value
 
 				if (countryId) {
-					console.log('setting countryId')
 					// @ts-expect-error -> string is okay.
 					form.setValue(countrySelectProps.name, countryId)
 					if (countrySelectProps.onChange && typeof countrySelectProps.onChange === 'function') {
 						countrySelectProps.onChange(countryId)
 					}
 				} else if (!phoneCountry && formError[phoneEntryProps.name]) {
-					console.log('clearing error')
 					form.resetField(countrySelectProps.name)
 					form.clearErrors(phoneEntryProps.name)
-					// // @ts-expect-error -> null is okay.
-					// form.setValue(countrySelectProps.name, null)
 				} else if (phoneCountry && !countryId) {
-					console.log('setting error')
 					form.setError(
 						phoneEntryProps.name,
 						{ message: `Country not active: ${phoneCountry}` },
@@ -176,26 +142,7 @@ export const PhoneNumberEntry = <T extends FieldValues>({
 				rightSection={countrySelection}
 				rightSectionWidth={56}
 				classNames={phoneEntryClasses}
-				// value={parsePhoneNumber(String(phoneValue), DEFAULT_COUNTRY)?.number}
-				// onChange={(e) =>
-				// 	onPhoneChange && typeof onPhoneChange === 'function' ? onPhoneChange(e) : undefined
-				// }
 				control={form.control}
-				// error={form.getFieldState(phoneEntryProps.name).error?.message}
-				// rules={{
-				// 	validate: (val) => {
-				// 		if (val) {
-				// 			const formatter = phoneFormatter.input(val)
-				// 			console.log('formatter', formatter)
-				// 		}
-				// 		const derivedCountry = phoneFormatter.getNumber()?.country
-				// 		console.log('val', val)
-				// 		console.log('derivedCountry', derivedCountry)
-				// 		const result = !!countryList.find(({ data }) => data.cca2 === phoneFormatter.getNumber()?.country)
-				// 		console.log('validator result', result)
-				// 		return result
-				// 	},
-				// }}
 				// @ts-expect-error -> name prop is okay.
 				error={<ErrorMessage errors={form.formState.errors} name={phoneEntryProps.name} as='span' />}
 				{...phoneEntryProps}
