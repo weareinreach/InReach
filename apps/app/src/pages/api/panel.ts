e627c9b3a313cca6902be32f69546a0aa7541539
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-11-06 14:48:28 -0500

    lazy load

--- apps/app/src/pages/api/panel.ts
+++ apps/app/src/pages/api/panel.ts
@@ -1,12 +1,11 @@
 import { type NextApiRequest, type NextApiResponse } from 'next'
-import { renderTrpcPanel } from 'trpc-panel'
 
 import { appRouter } from '@weareinreach/api'
 import { getEnv } from '@weareinreach/env'
 
-// eslint-disable-next-line @typescript-eslint/require-await
 export default async function handler(_: NextApiRequest, res: NextApiResponse) {
 	if (getEnv('NODE_ENV') === 'development') {
+		const { renderTrpcPanel } = await import('trpc-panel')
 		res.status(200).send(
 			renderTrpcPanel(appRouter, {
 				url: 'http://localhost:3000/api/trpc',
