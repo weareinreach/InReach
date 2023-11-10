bd5bceef4193ff5a17191b08f5a0eebcda3b9e2f
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-11-09 19:29:05 -0500

    fix bundling error

--- apps/app/sentry.edge.config.ts
+++ apps/app/sentry.edge.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable node/no-process-env */
 // This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
 // The config you add here will be used whenever one of the edge features is loaded.
 // Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
@@ -5,13 +6,13 @@
 
 import * as Sentry from '@sentry/nextjs'
 
+const isVercelProd = process.env.VERCEL_ENV === 'production'
 Sentry.init({
 	dsn: 'https://3398c2248c86498ab42fa8533e4f83f1@o1412293.ingest.sentry.io/6751163',
 
 	// Adjust this value in production, or use tracesSampler for greater control
-	tracesSampleRate: 0.5,
+	tracesSampleRate: isVercelProd ? 0.5 : 1.0,
 
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
-	// eslint-disable-next-line node/no-process-env
 	debug: !!process.env.SENTRY_DEBUG,
 })
--- apps/app/sentry.server.config.ts
+++ apps/app/sentry.server.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable node/no-process-env */
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
@@ -7,15 +8,16 @@ import { ProfilingIntegration } from '@sentry/profiling-node'
 
 import { prisma } from '@weareinreach/db'
 
+const isVercelProd = process.env.VERCEL_ENV === 'production'
+
 Sentry.init({
 	dsn: 'https://3398c2248c86498ab42fa8533e4f83f1@o1412293.ingest.sentry.io/6751163',
 
 	// Adjust this value in production, or use tracesSampler for greater control
-	tracesSampleRate: 0.5,
-	profilesSampleRate: 0.5,
+	tracesSampleRate: isVercelProd ? 0.5 : 1.0,
+	profilesSampleRate: isVercelProd ? 0.5 : 1.0,
 
 	// Setting this option to true will print useful information to the console while you're setting up Sentry.
-	// eslint-disable-next-line node/no-process-env
 	debug: !!process.env.SENTRY_DEBUG,
 	// instrumenter: 'otel',
 	integrations: [
