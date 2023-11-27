693c195f4618768d9abf4e31f1c5a437da9da220
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2023-11-27 14:24:08 -0500

    data migration -> ad hoc updates

--- packages/db/prisma/data-migrations/index.ts
+++ packages/db/prisma/data-migrations/index.ts
@@ -62,5 +62,6 @@ export * from './2023-11-17_add-missing-websites/index'
 export * from './2023-11-17_data-load/index'
 export * from './2023-11-20_fix-listing'
 export * from './2023-11-21_add-missing-phones/index'
+export * from './2023-11-22_ad-hoc-updates/index'
 export * from './2023-11-22_update-alert-messages/index'
 // codegen:end
