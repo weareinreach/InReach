752886da6aa98a5a33ae857934a817aa84574561
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-01-26 12:43:44 -0500

    create hook-form variant

--- packages/ui/components/data-portal/MultiSelectPopover/index.tsx
+++ packages/ui/components/data-portal/MultiSelectPopover/index.tsx
@@ -15,30 +15,7 @@ import { type CSSProperties, useCallback, useEffect, useMemo } from 'react'
 
 import { Icon } from '~ui/icon'
 
-const useStyles = createStyles(
-	(theme, { selectedCount, dimmed }: { selectedCount: number; dimmed: boolean }) => ({
-		count: {
-			...theme.other.utilityFonts.utility1,
-			background: dimmed ? theme.other.colors.secondary.darkGray : theme.other.colors.secondary.black,
-			borderRadius: '100%',
-			color: theme.other.colors.secondary.white,
-			width: rem(24),
-			height: rem(24),
-			textAlign: 'center',
-			display: 'inline-block',
-			verticalAlign: 'center',
-			lineHeight: 1.5,
-			opacity: selectedCount < 1 ? 0 : 1,
-		},
-		button: {
-			padding: `${rem(14)} ${rem(16)}`,
-			backgroundColor: theme.other.colors.secondary.white,
-			borderRadius: rem(8),
-			border: `${theme.other.colors.tertiary.coolGray} ${rem(1)} solid`,
-			height: rem(48),
-		},
-	})
-)
+import { useStyles } from './styles'
 
 export const MultiSelectPopover = ({
 	data,
