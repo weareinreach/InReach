b85389ee7c878e14b59f770deb4e9bd6a5747313
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-02-01 15:32:42 -0500

    tRPC handler importer

--- packages/api/lib/trpc.ts
+++ packages/api/lib/trpc.ts
@@ -22,3 +22,46 @@ export const staffProcedure = baseProcedure.use(isStaff)
 /** Permissioned router */
 export const permissionedProcedure = (procedure: PermissionedProcedure) =>
 	baseProcedure.use(hasPermissions).meta(getPermissions(procedure))
+
+/** Handler Cache */
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+const HANDLER_CACHE: Record<string, Function> = {}
+
+/**
+ * This function will import the module defined in importer just once and then cache the default export of
+ * that module.
+ *
+ * It gives you the default export of the module.
+ *
+ * **Note: It is your job to ensure that the name provided is unique across all routes.**
+ *
+ * @example
+ *
+ * ```ts
+ * const handler = await importHandler('myUniqueNameSpace', () => import('./getUser.handler'))
+ * return handler({ ctx, input })
+ * ```
+ */
+export const importHandler = async <
+	T extends {
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		default: Function
+	},
+>(
+	/**
+	 * The name of the handler in cache. It has to be unique across all routes
+	 */
+	name: string,
+	importer: () => Promise<T>
+) => {
+	const nameInCache = name as keyof typeof HANDLER_CACHE
+
+	if (!HANDLER_CACHE[nameInCache]) {
+		const importedModule = await importer()
+		HANDLER_CACHE[nameInCache] = importedModule.default
+		return importedModule.default as T['default']
+	}
+
+	return HANDLER_CACHE[nameInCache] as unknown as T['default']
+}
