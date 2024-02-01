bcdfad1dbc55453197298c150f818e902350ad76
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-02-01 15:33:09 -0500

    update internalNote router

--- packages/api/router/internalNote/index.ts
+++ packages/api/router/internalNote/index.ts
@@ -1,42 +1,34 @@
-import { defineRouter, staffProcedure } from '~api/lib/trpc'
+import { defineRouter, importHandler, staffProcedure } from '~api/lib/trpc'
 
 import * as schema from './schemas'
 
-const HandlerCache: Partial<InternalNoteHandlerCache> = {}
+const NAMESPACE = 'internalNote'
 
-type InternalNoteHandlerCache = {
-	byId: typeof import('./query.byId.handler').byId
-	getAllForRecord: typeof import('./query.getAllForRecord.handler').getAllForRecord
-	create: typeof import('./mutation.create.handler').create
-}
+const namespaced = (s: string) => `${NAMESPACE}.${s}`
 export const internalNoteRouter = defineRouter({
 	byId: staffProcedure
 		.meta({ hasPerm: 'internalNotesRead' })
 		.input(schema.ZByIdSchema)
-		.query(async ({ ctx, input }) => {
-			if (!HandlerCache.byId) HandlerCache.byId = await import('./query.byId.handler').then((mod) => mod.byId)
-			if (!HandlerCache.byId) throw new Error('Failed to load handler')
-			return HandlerCache.byId({ ctx, input })
+		.query(async (opts) => {
+			const handler = await importHandler(namespaced('byId'), () => import('./query.byId.handler'))
+			return handler(opts)
 		}),
 	getAllForRecord: staffProcedure
 		.meta({ hasPerm: 'internalNotesRead' })
 		.input(schema.ZGetAllForRecordSchema)
-		.query(async ({ ctx, input }) => {
-			if (!HandlerCache.getAllForRecord)
-				HandlerCache.getAllForRecord = await import('./query.getAllForRecord.handler').then(
-					(mod) => mod.getAllForRecord
-				)
-			if (!HandlerCache.getAllForRecord) throw new Error('Failed to load handler')
-			return HandlerCache.getAllForRecord({ ctx, input })
+		.query(async (opts) => {
+			const handler = await importHandler(
+				namespaced('getAllForRecord'),
+				() => import('./query.getAllForRecord.handler')
+			)
+			return handler(opts)
 		}),
 
 	create: staffProcedure
 		.meta({ hasPerm: 'internalNotesWrite' })
-		.input(schema.ZCreateSchema().inputSchema)
-		.mutation(async ({ ctx, input }) => {
-			if (!HandlerCache.create)
-				HandlerCache.create = await import('./mutation.create.handler').then((mod) => mod.create)
-			if (!HandlerCache.create) throw new Error('Failed to load handler')
-			return HandlerCache.create({ ctx, input })
+		.input(schema.ZCreateSchema)
+		.mutation(async (opts) => {
+			const handler = await importHandler(namespaced('create'), () => import('./mutation.create.handler'))
+			return handler(opts)
 		}),
 })
