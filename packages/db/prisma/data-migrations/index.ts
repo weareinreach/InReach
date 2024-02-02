d4be89c4ed96d0a515e0655fa9d14f0551789d25
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-02-02 10:21:12 -0500

    migration: deactivate incompatible-info attrib

--- packages/db/prisma/data-migrations/index.ts
+++ packages/db/prisma/data-migrations/index.ts
@@ -1,3 +1,4 @@
 // codegen:start {preset: barrel, include: ./**/*.ts, exclude: ['**/{!,___}*.ts']}
 export * from './2024-02-01_add-missing-attributes/index'
+export * from './2024-02-02_deactivate-incompatible-attribs'
 // codegen:end
