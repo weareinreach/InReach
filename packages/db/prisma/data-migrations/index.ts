3b2e3919090fbdbc8f5decc9401caababfd5af1d
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-02-14 16:39:06 -0500

    schema updates

--- packages/db/prisma/data-migrations/index.ts
+++ packages/db/prisma/data-migrations/index.ts
@@ -3,4 +3,5 @@ export * from './2024-01-31_fix-attr-supp-json/index'
 export * from './2024-01-31_target-population-attrib'
 export * from './2024-02-01_add-missing-attributes/index'
 export * from './2024-02-02_deactivate-incompatible-attribs'
+export * from './2024-02-14_attribute-supplement-schemas/index'
 // codegen:end
