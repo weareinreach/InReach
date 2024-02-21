16235c8f381379af225fd610be8648362a139e37
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-02-21 18:44:31 -0500

    data load

--- packages/db/prisma/data-migrations/index.ts
+++ packages/db/prisma/data-migrations/index.ts
@@ -3,4 +3,6 @@ export * from './2024-01-31_fix-attr-supp-json/index'
 export * from './2024-01-31_target-population-attrib'
 export * from './2024-02-01_add-missing-attributes/index'
 export * from './2024-02-02_deactivate-incompatible-attribs'
+export * from './2024-02-19_attach-orphan-text'
+export * from './2024-02-20_appsheet-load/index'
 // codegen:end
