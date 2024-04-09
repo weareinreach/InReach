e9130ffb11cb1325b754ca6c75ce65155b85bd57
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-04-09 14:39:19 -0400

    fix website update handling

--- packages/api/router/orgWebsite/mutation.update.handler.ts
+++ packages/api/router/orgWebsite/mutation.update.handler.ts
@@ -1,15 +1,38 @@
+import * as R from 'remeda'
+
 import { getAuditedClient } from '@weareinreach/db'
 import { type TRPCHandlerParams } from '~api/types/handler'
 
 import { type TUpdateSchema } from './mutation.update.schema'
 
 export const update = async ({ ctx, input }: TRPCHandlerParams<TUpdateSchema, 'protected'>) => {
-	const { where, data } = input
 	const prisma = getAuditedClient(ctx.actorId)
-	const updated = await prisma.orgWebsite.update({
+	const {
 		where,
 		data,
-	})
-	return updated
+		data: { url },
+	} = input
+	console.log(input)
+	if (R.isString(url)) {
+		const { locations, ...rest } = data
+		const upserted = await prisma.orgWebsite.upsert({
+			where,
+			create: {
+				id: where.id,
+				url,
+				...(locations && { locations: { create: locations.upsert.create } }),
+				...rest,
+			},
+			update: data,
+		})
+
+		return upserted
+	} else {
+		const updated = await prisma.orgWebsite.update({
+			where,
+			data,
+		})
+		return updated
+	}
 }
 export default update
