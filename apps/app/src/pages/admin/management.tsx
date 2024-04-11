4172df521751fd67d1a6699250e08a8686b53b60
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-04-11 12:14:44 -0400

    remove old mock

--- apps/app/src/pages/admin/management.tsx
+++ apps/app/src/pages/admin/management.tsx
@@ -1,13 +1,7 @@
 import { ManagementTable } from '@weareinreach/ui/components/data-portal/ManagementTable'
-import { userManagement } from '@weareinreach/ui/mockData/userManagement'
 
 export const Management = () => {
-	return (
-		<ManagementTable
-			data={userManagement.forUserManagementTable()}
-			columns={userManagement.forUserManagementColumns()}
-		></ManagementTable>
-	)
+	return <ManagementTable />
 }
 
 export default Management
