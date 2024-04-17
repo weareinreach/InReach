f5483222fdf75f71eb8ebef4089906d2eddb2e3b
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-04-17 10:28:23 -0400

    cleanup

--- packages/api/router/savedLists/mutation.create.handler.ts
+++ packages/api/router/savedLists/mutation.create.handler.ts
@@ -1,9 +1,7 @@
-import { type z } from 'zod'
-
 import { getAuditedClient } from '@weareinreach/db'
 import { type TRPCHandlerParams } from '~api/types/handler'
 
-import { type TCreateSchema, ZCreateSchema } from './mutation.create.schema'
+import { type TCreateSchema } from './mutation.create.schema'
 
 export const create = async ({ ctx, input }: TRPCHandlerParams<TCreateSchema, 'protected'>) => {
 	const prisma = getAuditedClient(ctx.actorId)
