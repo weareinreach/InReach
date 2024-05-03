69736b10ff519da543c30d7e14e1137699a5e123
Joe Karow <58997957+JoeKarow@users.noreply.github.com>
2024-05-03 12:00:37 -0400

    add error handling

--- packages/api/router/organization/query.forOrgPage.handler.ts
+++ packages/api/router/organization/query.forOrgPage.handler.ts
@@ -1,51 +1,56 @@
 import { prisma } from '@weareinreach/db'
+import { handleError } from '~api/lib/errorHandler'
 import { attributes, freeText, isPublic } from '~api/schemas/selects/common'
 import { type TRPCHandlerParams } from '~api/types/handler'
 
 import { type TForOrgPageSchema } from './query.forOrgPage.schema'
 
 const forOrgPage = async ({ input }: TRPCHandlerParams<TForOrgPageSchema>) => {
-	const { slug } = input
-	const org = await prisma.organization.findUniqueOrThrow({
-		where: {
-			slug,
-			...isPublic,
-		},
-		select: {
-			id: true,
-			name: true,
-			slug: true,
-			published: true,
-			lastVerified: true,
-			allowedEditors: { where: { authorized: true }, select: { userId: true } },
-			description: freeText,
-
-			reviews: {
-				where: { visible: true, deleted: false },
-				select: { id: true },
+	try {
+		const { slug } = input
+		const org = await prisma.organization.findUniqueOrThrow({
+			where: {
+				slug,
+				...isPublic,
 			},
-			locations: {
-				where: isPublic,
-				select: {
-					id: true,
-					street1: true,
-					street2: true,
-					city: true,
-					postCode: true,
-					country: { select: { cca2: true } },
-					govDist: { select: { abbrev: true, tsKey: true, tsNs: true } },
-					notVisitable: true,
+			select: {
+				id: true,
+				name: true,
+				slug: true,
+				published: true,
+				lastVerified: true,
+				allowedEditors: { where: { authorized: true }, select: { userId: true } },
+				description: freeText,
+
+				reviews: {
+					where: { visible: true, deleted: false },
+					select: { id: true },
 				},
+				locations: {
+					where: isPublic,
+					select: {
+						id: true,
+						street1: true,
+						street2: true,
+						city: true,
+						postCode: true,
+						country: { select: { cca2: true } },
+						govDist: { select: { abbrev: true, tsKey: true, tsNs: true } },
+						notVisitable: true,
+					},
+				},
+				attributes,
 			},
-			attributes,
-		},
-	})
-	const { allowedEditors, ...orgData } = org
-	const reformatted = {
-		...orgData,
-		isClaimed: Boolean(allowedEditors.length),
-	}
+		})
+		const { allowedEditors, ...orgData } = org
+		const reformatted = {
+			...orgData,
+			isClaimed: Boolean(allowedEditors.length),
+		}
 
-	return reformatted
+		return reformatted
+	} catch (err) {
+		return handleError(err)
+	}
 }
 export default forOrgPage
