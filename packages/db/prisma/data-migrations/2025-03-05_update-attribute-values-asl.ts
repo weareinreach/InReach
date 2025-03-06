923bb7a08d170444ca2af40eb26ac923c994c79a
trigal <keeplist1@gmail.com>
2025-03-05 16:08:07 -0800

    migration files to update Attribute table values

---
+++ packages/db/prisma/data-migrations/2025-03-05_update-attribute-values-asl.ts
@@ -0,0 +1,50 @@
+import { type MigrationJob } from '~db/prisma/dataMigrationRunner'
+import { type JobDef } from '~db/prisma/jobPreRun'
+
+/** Define the job metadata here. */
+const jobDef: JobDef = {
+	jobId: '2025-03-05_update-attribute-values-asl',
+	title: 'change the name values for ASL',
+	createdBy: 'Diana Garbarino',
+	/** Optional: Longer description for the job */
+	description: 'change the name values for ASL',
+}
+/**
+ * Job export - this variable MUST be UNIQUE
+ */
+export const job20250305_update_attribute_values_asl = {
+	title: `[${jobDef.jobId}] ${jobDef.title}`,
+	task: async (ctx, task) => {
+		const { createLogger, formatMessage, jobPostRunner, prisma } = ctx
+		/** Create logging instance */
+		createLogger(task, jobDef.jobId)
+		const log = (...args: Parameters<typeof formatMessage>) => (task.output = formatMessage(...args))
+
+		// Variables for the update
+		const key = 'lang.american-sign-language'
+		const ns = 'attribute'
+		const newTextValue = 'American Sign Language (ASL)'
+		// Perform the update
+		const update1 = await prisma.translationKey.update({
+			where: { ns_key: { key, ns } },
+			data: { text: newTextValue },
+		})
+
+		log(`Updated translationKey: ${update1.key} with new text: "${update1.text}"`)
+
+		const update2 = await prisma.attribute.updateMany({
+			where: { tsKey: key, tsNs: ns },
+			data: { name: newTextValue },
+		})
+
+		log(`Updated ${update2.count} attributes with new name: "${newTextValue}"`)
+
+		/**
+		 * DO NOT REMOVE BELOW
+		 *
+		 * This writes a record to the DB to register that this migration has run successfully.
+		 */
+		await jobPostRunner(jobDef)
+	},
+	def: jobDef,
+} satisfies MigrationJob
