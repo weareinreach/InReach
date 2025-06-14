5bc60c65acf374a14fedfc7dd02bf8853154d902
trigal <keeplist1@gmail.com>
2025-06-13 21:01:26 -0700

    getOrg download for csv buttons

--- packages/ui/components/data-portal/OrganizationTable.tsx
+++ packages/ui/components/data-portal/OrganizationTable.tsx
@@ -16,6 +16,7 @@ import { type Route } from 'nextjs-routes'
 import { type Dispatch, type SetStateAction, useEffect, useMemo, useRef, useState } from 'react'
 
 import { type ApiOutput } from '@weareinreach/api'
+import { CsvDownload } from '~ui/components/core/ActionButtons/CsvDownload'
 import { Link } from '~ui/components/core/Link'
 import { useCustomVariant } from '~ui/hooks/useCustomVariant'
 import { Icon } from '~ui/icon'
@@ -75,6 +76,20 @@ const ToolbarButtons = ({ columnFilters, setColumnFilters }: ToolbarButtonsProps
 
 	return (
 		<Group>
+			<Group noWrap spacing={8}>
+				<CsvDownload
+					label='All Published Organizations'
+					fileName='all_published_organizations.csv'
+					useMutationHook={() => api.organization.getAllPublishedForCSV.useMutation()}
+					permissionKey='dataPortalManager'
+				/>
+				<CsvDownload
+					label='All Unpublished Organizations'
+					fileName='all_unpublished_organizations.csv'
+					useMutationHook={() => api.organization.getAllUnpublishedForCSV.useMutation()}
+					permissionKey='dataPortalManager'
+				/>
+			</Group>
 			<Tooltip
 				label={
 					publishedState
