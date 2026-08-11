9388b29ac131a88817fea945ca24d55526e5c3ae
trigal2012 <keeplist1@gmail.com>
2026-08-11 13:21:38 -0700

    saved list now appers without refresh

--- packages/api/lib/middleware/permissions.ts
+++ packages/api/lib/middleware/permissions.ts
@@ -1,7 +1,7 @@
 import { TRPCError } from '@trpc/server'
 import invariant from 'tiny-invariant'
 
-import { type Context } from '../context'
+import { type Context, markSkipCache } from '../context'
 import { type Meta, t } from '../initTRPC'
 
 /** Send unauthorized rejection via middleware */
@@ -103,6 +103,7 @@ export const isAdmin = t.middleware(({ ctx, next }) => {
 		return reject()
 	}
 
+	markSkipCache(ctx.res)
 	return next({
 		ctx: {
 			...ctx,
@@ -126,6 +127,7 @@ export const isStaff = t.middleware(({ ctx, meta, next }) => {
 		return reject()
 	}
 
+	markSkipCache(ctx.res)
 	return next({
 		ctx: {
 			...ctx,
@@ -143,6 +145,7 @@ export const hasPermissions = t.middleware(({ ctx, meta, next }) => {
 	if (ctx.session === null) return reject()
 
 	if (checkPermissions(meta, ctx)) {
+		markSkipCache(ctx.res)
 		return next({
 			ctx: {
 				...ctx,
